b589402cad30f4d0fc0ebbc1cdbc30b821d31283
chenww <chenww@hzcominfo.com>
2016-06-15 14:33:36 +0800

    first

---
+++ src/main/java/com/hzcominfo/tools/kafkaconsumer/thread/ConsumerThread.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hzcominfo.tools.kafkaconsumer.thread;
+
+import com.hzcominfo.tools.kafkaconsumer.context.DataContext;
+import com.hzcominfo.tools.kafkaconsumer.mapper.KafkaMessage;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import kafka.consumer.KafkaStream;
+import kafka.message.MessageAndMetadata;
+import org.bson.BSONDecoder;
+import org.bson.BSONObject;
+import org.bson.BasicBSONDecoder;
+
+/**
+ *
+ * @author hzcominfo
+ */
+public class ConsumerThread extends Thread {
+
+    private KafkaStream<byte[], byte[]> stream;
+    private int msgListMax;
+    private String keyFiled;
+    private Map<String, String> topicKeyFieldMap;
+
+    public void setTopicKeyFieldMap(Map<String, String> topicKeyFieldMap) {
+        this.topicKeyFieldMap = topicKeyFieldMap;
+    }
+
+    public void setKeyFiled(String keyFiled) {
+        this.keyFiled = keyFiled;
+    }
+
+    public void setMsgListMax(int msgListMax) {
+        this.msgListMax = msgListMax;
+    }
+
+    public void setStream(KafkaStream<byte[], byte[]> stream) {
+        this.stream = stream;
+    }
+
+    @Override
+    public void run() {
+        if (stream != null) {
+            BSONDecoder decoder = new BasicBSONDecoder();
+            for (MessageAndMetadata<byte[], byte[]> msgAndMetadata : stream) {
+                String topic = msgAndMetadata.topic();
+                if (keyFiled == null) {
+                    keyFiled = topicKeyFieldMap.get(topic);
+                }
+                byte[] message = msgAndMetadata.message();
+                BSONObject value = decoder.readObject(message);
+                KafkaMessage kafkaMsg = new KafkaMessage();
+                kafkaMsg.setTopic(topic);
+                kafkaMsg.setValues((Map<String, Object>) value.get("value"));
+                if (kafkaMsg.getValues().containsKey(keyFiled)) {
+                    kafkaMsg.setKeyValue(kafkaMsg.getValues().get(keyFiled));
+                }
+                while (DataContext.msgList.size() > msgListMax) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(ConsumerThread.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+
+                DataContext.msgList.add(kafkaMsg);
+
+            }
+        }
+    }
+
+}
