1cafe228eebed58d398496bb88eed573ffd9105b
zx <zhangx@hzcominfo.com>
2016-09-11 23:28:15 +0800

    adjust dao base classes, simple first.

--- core/src/main/java/net/butfly/albacore/dao/BasicDAO.java
+++ core/src/main/java/net/butfly/albacore/dao/BasicDAO.java
@@ -7,11 +7,7 @@ import net.butfly.albacore.dbo.criteria.Page;
 import net.butfly.albacore.entity.AbstractEntity;
 
 @SuppressWarnings("unchecked")
-public interface EntityDAO extends EntityBasicDAO {
-	<K extends Serializable, E extends AbstractEntity<K>> K insert(final E entity);
-
-	<K extends Serializable, E extends AbstractEntity<K>> K[] insert(final E... entity);
-
+public interface BasicDAO extends SimpleDAO {
 	<K extends Serializable, E extends AbstractEntity<K>> E delete(final Class<E> entityClass, final K key);
 
 	<K extends Serializable, E extends AbstractEntity<K>> E[] delete(final Class<E> entityClass, final K... key);
@@ -19,10 +15,6 @@ public interface EntityDAO extends EntityBasicDAO {
 	<K extends Serializable, E extends AbstractEntity<K>> E update(final E entity);
 
 	<K extends Serializable, E extends AbstractEntity<K>> E[] update(final E... entity);
-
-	<K extends Serializable, E extends AbstractEntity<K>> E select(final Class<E> entityClass, final K key);
-
-	<K extends Serializable, E extends AbstractEntity<K>> E[] select(final Class<E> entityClass, final K... key);
 
 	<K extends Serializable, E extends AbstractEntity<K>> E[] delete(final Class<E> entityClass, Criteria criteria);
 
