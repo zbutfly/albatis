32593ee1cb7e2f31b1980f9361aae7ff04b2d9a5
zx <zhangx@hzcominfo.com>
2016-09-12 16:46:17 +0800

    add docs, add test case

---
+++ kafka/src/test/java/net/butfly/albatis/dao/User.java
@@ -0,0 +1,7 @@
+package net.butfly.albatis.dao;
+
+import net.butfly.albacore.entity.Entity;
+
+public class User extends Entity<String> {
+	private static final long serialVersionUID = 3948589400738873410L;
+}
