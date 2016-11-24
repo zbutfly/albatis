b91b3484d163495bacb2a6c435af82bb5fa4df50
कलविङ्क <zhangx@hzcominfo.com>
2016-11-24 19:38:40 +0800

    now KafkaInput ok all!!!!

--- kafka-io/src/test/java/net/butfly/albatis/dao/KafkaTest.java
+++ kafka-io/src/test/java/net/butfly/albatis/dao/KafkaTest.java
@@ -6,15 +6,18 @@ import java.util.Map;
 
 import net.butfly.albacore.exception.ConfigException;
 import net.butfly.albatis.kafka.KafkaInput;
+import net.butfly.albatis.kafka.KafkaMessage;
 
 public class KafkaTest {
 	public static void main(String[] args) throws ConfigException, IOException {
 		Map<String, Integer> topics = new HashMap<>();
-		topics.put("HZGA_GAZHK_CZRK", 1);
+		topics.put("HZGA_GAZHK_CZRK", 3);
+		topics.put("HZGA_GAZHK_LGY_NB", 3);
 		try (KafkaInput in = new KafkaInput("KafkaInput", "kafka.properties", topics);) {
-			//			in.enqueue0(it);
-
+			do {
+				for (KafkaMessage m : in.dequeue(100))
+					System.err.println("Fetched: " + m.toString());
+			} while (true);
 		}
-
 	}
 }
