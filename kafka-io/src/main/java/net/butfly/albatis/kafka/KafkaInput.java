2f865a0587dbaa79f7575beddb39fd62c79f7c33
zx <zhangx@hzcominfo.com>
2016-11-24 19:38:40 +0800

    now KafkaInput ok all!!!!

--- kafka-io/src/main/java/net/butfly/albatis/kafka/KafkaInput.java
+++ kafka-io/src/main/java/net/butfly/albatis/kafka/KafkaInput.java
@@ -1,22 +1,20 @@
 package net.butfly.albatis.kafka;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import com.google.common.collect.Lists;
-
 import kafka.consumer.Consumer;
-import kafka.consumer.ConsumerConfig;
 import kafka.consumer.ConsumerIterator;
 import kafka.consumer.KafkaStream;
 import kafka.javaapi.consumer.ConsumerConnector;
 import net.butfly.albacore.exception.ConfigException;
 import net.butfly.albacore.io.MapInput;
 import net.butfly.albacore.io.queue.Q;
-import net.butfly.albacore.io.queue.SimpleQ;
 import net.butfly.albacore.utils.Collections;
 import net.butfly.albacore.utils.async.Concurrents;
 import net.butfly.albacore.utils.logger.Logger;
@@ -26,7 +24,7 @@ public class KafkaInput extends MapInput<String, KafkaMessage> {
 	private static final long serialVersionUID = 7617065839861658802L;
 	private static final Logger logger = Logger.getLogger(KafkaInput.class);
 	private final ConsumerConnector connect;
-	private final Map<String, SimpleQ<KafkaMessage>> queues;
+	private final Map<String, List<Integer>> order;
 	private final Map<String, List<KafkaStream<byte[], byte[]>>> topicStreams;
 	// private KafkaConsumer<byte[], byte[]> consumer;
 
@@ -36,30 +34,58 @@ public class KafkaInput extends MapInput<String, KafkaMessage> {
 
 	public KafkaInput(String name, final KafkaInputConfig config, final Map<String, Integer> topics) throws ConfigException {
 		super(name);
-		connect = connect(config.getConfig());
-		queues = new HashMap<>();
-		for (String topic : topics.keySet())
-			queues.put(topic, new SimpleQ<KafkaMessage>(name() + "_LOCAL_POOL_" + topic, 10000));
+		order = new HashMap<>();
+		for (String topic : topics.keySet()) {
+			Integer[] ints = new Integer[topics.get(topic)];
+			for (int i = 0; i < ints.length; i++)
+				ints[i] = i;
+			List<Integer> l = Arrays.asList(ints);
+			order.put(topic, l);
+		}
 
-		long fucking = Long.parseLong(System.getProperty("albatis.kafka.fucking.waiting", "15000"));
+		logger.debug("Kafka [" + config.toString() + "] connecting.");
+		connect = Consumer.createJavaConsumerConnector(config.getConfig());
+		logger.debug("Kafka [" + config.toString() + "] Connected.");
+		topicStreams = connect.createMessageStreams(topics);
 
-		this.topicStreams = connect.createMessageStreams(topics);
-		logger.error("FFFFFFFucking lazy initialization of Kafka, we are sleeping [" + fucking + "ms].");
-		Concurrents.waitSleep(fucking);
-		logger.error("We had waked up, are you ok, Kafka?");
+//		long fucking = Long.parseLong(System.getProperty("albatis.kafka.fucking.waiting", "15000"));
+//		logger.error("FFFFFFFucking lazy initialization of Kafka, we are sleeping [" + fucking + "ms].");
+//		Concurrents.waitSleep(fucking);
+//		logger.error("We had waked up, are you ok, Kafka?");
 		logger.debug("KafkaInput ready in [" + topicStreams.size() + "] topics: " + topics.toString());
 	}
 
 	@Override
 	public KafkaMessage dequeue0(String topic) {
-		SimpleQ<KafkaMessage> q = queues.get(topic);
-		KafkaMessage m = q.dequeue0();
-		if (null != m) return m;
-		ConsumerIterator<byte[], byte[]> it = topicStreams.get(topic).get(0).iterator();
-		if (it.hasNext()) {
-			q.enqueue(Collections.transform(Lists.newArrayList(it), meta -> new KafkaMessage(meta)));
-			return q.dequeue0();
-		} else return null;
+		List<KafkaStream<byte[], byte[]>> l = topicStreams.get(topic);
+		for (Integer i : order.compute(topic, (k, v) -> Collections.disorderize(v))) {
+			ConsumerIterator<byte[], byte[]> it = l.get(i).iterator();
+			if (it.hasNext()) return new KafkaMessage(it.next());
+		}
+		return null;
+	}
+
+	@Override
+	public List<KafkaMessage> dequeue(long batchSize, String... topic) {
+		List<KafkaMessage> batch = new ArrayList<>();
+		long prev;
+		do {
+			prev = batch.size();
+			for (String t : topic) {
+				for (KafkaStream<byte[], byte[]> s : topicStreams.get(t)) {
+					ConsumerIterator<byte[], byte[]> it = s.iterator();
+					if (it.hasNext()) batch.add(new KafkaMessage(it.next()));
+				}
+			}
+			if (batch.size() >= batchSize) return batch;
+			if (batch.size() == 0) Concurrents.waitSleep(EMPTY_WAIT_MS);
+		} while (batch.size() < batchSize && (prev != batch.size() || batch.size() == 0));
+		return batch;
+	}
+
+	@Override
+	public List<KafkaMessage> dequeue(long batchSize) {
+		return dequeue(batchSize, keys().toArray(new String[0]));
 	}
 
 	@Override
@@ -90,12 +116,5 @@ public class KafkaInput extends MapInput<String, KafkaMessage> {
 	@Override
 	public void close() {
 		connect.shutdown();
-	}
-
-	private ConsumerConnector connect(ConsumerConfig config) {
-		logger.debug("Kafka [" + config.zkConnect() + "] connecting (groupId: [" + config.groupId() + "]).");
-		ConsumerConnector conn = Consumer.createJavaConsumerConnector(config);
-		logger.debug("Kafka [" + config.zkConnect() + "] Connected (groupId: [" + config.groupId() + "]).");
-		return conn;
 	}
 }
