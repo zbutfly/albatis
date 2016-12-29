e1b19a6d77659edf4cde3c32672ed893de01470f
licheng <licheng@hzcominfo.com>
2016-12-29 11:36:25 +0800

    filterChain

---
+++ core/src/main/java/com/hzcominfo/albatis/search/filter/BasicFilterChain.java
@@ -0,0 +1,34 @@
+package com.hzcominfo.albatis.search.filter;
+
+import com.hzcominfo.albatis.search.exception.SearchAPIException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+public abstract class BasicFilterChain<Q,R> implements FilterChain<Q,R> {
+
+    private List<Filter<Q,R>> filterList = new ArrayList<>(4);
+    private FilterChain<Q,R> nextChain;
+    private volatile int index;
+
+    @Override
+    public void doFilter(Q query, R response) throws SearchAPIException {
+        if(index==filterList.size()){
+            return;
+        }
+        if(nextChain == null){
+            nextChain = this;
+        }
+        Filter<Q,R> filter = filterList.get(index);
+        index++;
+        filter.doFilter(query,response,nextChain);
+    }
+
+    public BasicFilterChain<Q,R> add(Filter<Q,R> filter){
+        filterList.add(filter);
+        return this;
+    }
+}
