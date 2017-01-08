c265563434c886f68b3d50d585a15073ce27b4d4
zx <zhangx@hzcominfo.com>
2017-01-09 02:09:14 +0800

    enable es conn/url

--- es-io/src/main/java/net/butfly/albatis/elastic/ElasticOutput.java
+++ es-io/src/main/java/net/butfly/albatis/elastic/ElasticOutput.java
@@ -6,33 +6,23 @@ import java.util.List;
 import org.elasticsearch.action.bulk.BulkItemResponse;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.update.UpdateRequest;
-import org.elasticsearch.client.transport.TransportClient;
 
 import net.butfly.albacore.io.Output;
 import net.butfly.albacore.utils.Systems;
 
 public class ElasticOutput extends Output<ElasticMessage> {
 	private static final long serialVersionUID = 1227554461265245482L;
-	protected final TransportClient elastic;
+	private final ElasticConnect conn;
 
-	public ElasticOutput(final String cluster, final String hostports) throws IOException {
-		this(cluster, hostports, 1);
-	}
-
-	public ElasticOutput(final String cluster, final String hostports, final int parallelism) throws IOException {
-		this(cluster, hostports, parallelism, 4096);
-	}
-
-	public ElasticOutput(final String cluster, final String hostports, final int parallelism, final int outputBatchBytes)
-			throws IOException {
-		super("elastic-output-queue");
-		elastic = Elastics.connect(cluster, hostports.split(","));
+	public ElasticOutput(String name, ElasticConnect conn) throws IOException {
+		super(name);
+		this.conn = conn;
 	}
 
 	@Override
 	public boolean enqueue0(ElasticMessage s) {
 		if (s == null) return false;
-		elastic.update(build(s)).actionGet();
+		conn.getClient().update(build(s)).actionGet();
 		return true;
 	}
 
@@ -42,7 +32,7 @@ public class ElasticOutput extends Output<ElasticMessage> {
 		for (ElasticMessage d : docs)
 			if (null != d) req.add(build(d));
 		long s = 0;
-		for (BulkItemResponse r : elastic.bulk(req).actionGet())
+		for (BulkItemResponse r : conn.getClient().bulk(req).actionGet())
 			if (!r.isFailed()) s++;
 		return s;
 	}
@@ -55,5 +45,11 @@ public class ElasticOutput extends Output<ElasticMessage> {
 		req.id(d.getId());
 		req.doc(d.getValues());
 		return req;
+	}
+
+	@Override
+	public void closing() {
+		super.closing();
+		conn.close();
 	}
 }
