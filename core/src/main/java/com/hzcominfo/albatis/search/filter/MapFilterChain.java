b660f20ae002de72f1ac537ff751a554b7b0dbca
诚 李 <licheng@localhost>
2017-01-10 14:14:20 +0800

    Merge branch 'develop/search' of http://10.118.137.167:2890/scm/alframs/albatis
    # Conflicts:
    #	core/src/main/java/com/hzcominfo/albatis/search/filter/FilterLoader.java

---
+++ core/src/main/java/com/hzcominfo/albatis/search/filter/MapFilterChain.java
@@ -0,0 +1,9 @@
+package com.hzcominfo.albatis.search.filter;
+
+import java.util.Map;
+
+/**
+ * 限定于只能用于过滤Map数据的FilterChain 输入和输出都是Map
+ */
+public class MapFilterChain<K extends Map, V extends Map> extends BasicFilterChain<K, V> {
+}
