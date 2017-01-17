20696a0be0760827ed0ac0d1e3f0d5ee51bb484b
langjx <langjx@hzcominfo.com>
2017-01-17 17:43:39 +0800

    spark sql pom

--- spark/sparksql/src/main/scala/com/hzcominfo/search/elastic/ElasticSparkSql.scala
+++ spark/sparksql/src/main/scala/com/hzcominfo/search/elastic/ElasticSparkSql.scala
@@ -3,19 +3,31 @@ package com.hzcominfo.search.elastic
 import org.apache.spark.sql.SparkSession
 
 object ElasticSparkSql {
-  def main(args: Array[String]) {
-    var session = SparkSession
-      .builder()
-      .appName("elasticTest")
-      .master("local")
-      .config("es.nodes", "10.118.159.45")
-      .config("es.port", "39200")
-      .config("es.read.field.as.array.include","true")
-      //.config("spark.sql.warehouse.dir", "file:///D:/albatis/spark-warehouse")
-      .getOrCreate()
 
-    val df = session.read.format("org.elasticsearch.spark.sql")
-      .load("net_police_201701/HZGA_WA_SOURCE_FJ_1001")
-    df.show()
+  def main(args: Array[String]) {
+    table()
+  }
+
+  def table(): Unit = {
+    val session = SparkSession.builder()
+      .appName("elasticTest")
+      .master("local[10]")
+      .config("spark.sql.warehouse.dir", "file:///D:/albatis/spark-warehouse")
+      .getOrCreate()
+    val op = Map("es.nodes" -> "10.118.159.45", "es.port" -> "39200", "es.scroll.size" -> "90000",
+      "es.read.field.as.array.include" -> "allPhones,callingidcards,calledidcards,idcards,receiptAddresss")
+
+    val df = session.read.format("org.elasticsearch.spark.sql").options(op)
+      .load("scattered_data_2017/phone_bill").limit(50000)
+    df.createOrReplaceTempView("t")
+
+    val df2 = session.read.format("org.elasticsearch.spark.sql").options(op)
+      .load("scattered_data/phone_bill_increment").limit(50000)
+    df2.createOrReplaceTempView("s")
+
+
+    val sql = session.sql("select t.callingAttr from t ,s where t.idcards = s.idcards")
+    sql.show(270000)
+
   }
 }
