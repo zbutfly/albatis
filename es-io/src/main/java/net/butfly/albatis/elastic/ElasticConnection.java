567ab8e393b498f7d58d028386ee035f4ee46596
langjx <langjx@hzcominfo.com>
2017-01-18 14:44:44 +0800

    es io uri

--- es-io/src/main/java/net/butfly/albatis/elastic/ElasticConnection.java
+++ es-io/src/main/java/net/butfly/albatis/elastic/ElasticConnection.java
@@ -15,44 +15,48 @@ import net.butfly.albacore.utils.Pair;
 import net.butfly.albacore.utils.logger.Logger;
 
 public class ElasticConnection extends NoSqlConnection<TransportClient> {
-	private static final Logger logger = Logger.getLogger(ElasticConnection.class);
-	protected ElasticURI elasticURI;
+    private static final Logger logger = Logger.getLogger(ElasticConnection.class);
 
-	public ElasticConnection(String connection) throws IOException {
-		super(new URISpec(connection), uri -> {
-			Settings.Builder settings = Settings.settingsBuilder();
-			settings.put(uri.getParameters());
-			settings.put("client.transport.ignore_cluster_name", true);
-			TransportClient c = TransportClient.builder().settings(settings).build();
-			for (Pair<String, Integer> h : uri.getHosts()) {
-				int port = h.value2() == null ? 39300 : h.value2();
-				try {
-					c.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(h.value1()), port));
-				} catch (UnknownHostException e) {
-					throw new RuntimeException(e);
-				}
-			}
-			return c;
-		}, "elasticsearch");
-		elasticURI = new ElasticURI(uri.toURI());
-	}
+    public ElasticConnection(String connection) throws IOException {
+        super(new URISpec(connection), uri -> {
+            Settings.Builder settings = Settings.settingsBuilder();
+            settings.put(uri.getParameters());
+            settings.put("client.transport.ignore_cluster_name", true);
+            TransportClient c = TransportClient.builder().settings(settings).build();
+            for (Pair<String, Integer> h : uri.getHosts()) {
+                int port = h.value2() == null ? 39300 : h.value2();
+                try {
+                    c.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(h.value1()), port));
+                } catch (UnknownHostException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return c;
+        }, "elasticsearch");
+    }
 
-	public ElasticConnection(String url, String... indexAndType) throws IOException {
-		this(url);
-		if (indexAndType.length > 2) logger.warn("only index and type parsed, other is ignore");
-	}
+    public ElasticConnection(String url, String... indexAndType) throws IOException {
+        this(url);
+        if (indexAndType.length > 2) logger.warn("only index and type parsed, other is ignore");
+    }
 
-	public String getDefaultIndex() {
-		return elasticURI.getIndex();
-	}
+    public String getDefaultIndex() {
+        String[] paths = uri.getPathSegs();
+        if (paths.length > 1)
+            return paths[0];
+        else return null;
+    }
 
-	public String getDefaultType() {
-		return elasticURI.getType();
-	}
+    public String getDefaultType() {
+        String[] paths = uri.getPathSegs();
+        if (paths.length > 2)
+            return paths[1];
+        else return null;
+    }
 
-	@Override
-	public void close() throws IOException {
-		super.close();
-		client().close();
-	}
+    @Override
+    public void close() throws IOException {
+        super.close();
+        client().close();
+    }
 }
--- es-io/src/main/java/net/butfly/albatis/elastic/ElasticInput.java
+++ es-io/src/main/java/net/butfly/albatis/elastic/ElasticInput.java
@@ -3,50 +3,76 @@ package net.butfly.albatis.elastic;
 import net.butfly.albacore.io.Input;
 import net.butfly.albacore.utils.logger.Logger;
 import org.elasticsearch.action.admin.indices.mapping.get.GetMappingsRequest;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.index.query.QueryBuilders;
 
 import java.io.IOException;
 import java.util.List;
 
-public class ElasticInput extends Input<GetMappingsRequest> {
-	private static final long serialVersionUID = -5666669099160512388L;
-	protected static final Logger logger = Logger.getLogger(ElasticInput.class);
-	private final ElasticConnection elastic;
+public class ElasticInput extends Input<SearchResponse> {
+    private static final long serialVersionUID = -5666669099160512388L;
+    protected static final Logger logger = Logger.getLogger(ElasticInput.class);
+    private final ElasticConnection elastic;
+    //view time def 10 minute
+    private int scrolltime = 10;
+    //scrollnumber
+    private int scrollnumber;
+    private SearchResponse searchResponse = null;
 
-	public ElasticInput(String connection) throws IOException {
-		super("elastic-input-queue");
-		elastic = new ElasticConnection(connection);
-		// if (null != filter && filter.length == 1 &&
-		// elastic.search(filter[0]).actionGet().status() != RestStatus.FOUND)
-		// throw new RuntimeException();
-	}
+    public ElasticInput(String connection) throws IOException {
+        super("elastic-input-queue");
+        elastic = new ElasticConnection(connection);
+    }
 
-	@Override
-	public void closing() {
-		super.closing();
-		try {
-			elastic.close();
-		} catch (IOException e) {
-			logger.error("Close failure", e);
-		}
-	}
+    @Override
+    public void closing() {
+        super.closing();
+        try {
+            elastic.close();
+        } catch (IOException e) {
+            logger.error("Close failure", e);
+        }
+    }
 
-	@Override
-	public long size() {
-		return 0;
-	}
+    @Override
+    public long size() {
+        return 0;
+    }
 
-	@Override
-	public boolean empty() {
-		return true;
-	}
+    @Override
+    public boolean empty() {
+        return true;
+    }
 
-	@Override
-	public GetMappingsRequest dequeue0() {
-		return null;
-	}
+    @Override
+    public SearchResponse dequeue0() {
+        if (searchResponse == null)
+            scanType();
+        else scanType2(searchResponse);
+        return searchResponse;
+    }
 
-	@Override
-	public List<GetMappingsRequest> dequeue(long batchSize) {
-		return super.dequeue(batchSize);
-	}
+    @Override
+    public List<SearchResponse> dequeue(long batchSize) {
+        return super.dequeue(batchSize);
+
+    }
+
+    @SuppressWarnings("deprecation")
+    public void scanType() {
+        SearchRequestBuilder searchRequest = elastic.client().prepareSearch("scattered_data").setSize(scrollnumber)
+                .setSearchType(SearchType.SCAN).setScroll(TimeValue.timeValueMinutes(scrolltime))
+                .setQuery(QueryBuilders.matchAllQuery());
+        logger.trace(searchRequest.toString());
+        searchResponse = searchRequest.execute().actionGet();
+    }
+
+    private SearchResponse scanType2(SearchResponse searchResponse) {
+        SearchResponse searchResponse2 = elastic.client().prepareSearchScroll(searchResponse.getScrollId())
+                .setScroll(TimeValue.timeValueMinutes(scrolltime)).execute().actionGet();
+        return searchResponse2;
+    }
 }
