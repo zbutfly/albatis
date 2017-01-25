9569808d826210b705b7b3bcdecea6b343c3fc1d
कलविङ्क <zhangx@hzcominfo.com>
2017-01-25 13:52:21 +0800

    add mongodb collection seg

--- mongodb-io/src/main/java/net/butfly/albatis/mongodb/MongoConnection.java
+++ mongodb-io/src/main/java/net/butfly/albatis/mongodb/MongoConnection.java
@@ -7,6 +7,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import com.hzcominfo.albatis.nosql.NoSqlConnection;
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 
@@ -16,11 +17,12 @@ import net.butfly.albacore.utils.logger.Logger;
 public class MongoConnection extends NoSqlConnection<MongoClient> {
 	private static final Logger logger = Logger.getLogger(MongoConnection.class);
 	private final Map<String, DB> dbs;
-	private String defaultDB;
+	private final String defaultDB;
+	private final String defaultCollection;
 
 	public MongoConnection(String connection) throws IOException {
 		super(new URISpec(connection), uri -> {
-			MongoClientURI u = new MongoClientURI(uri.toString());
+			MongoClientURI u = new MongoClientURI(uri.getScheme() + "://" + uri.getAuthority() + "/" + uri.getPathSegs()[0]);
 			try {
 				return new MongoClient(u);
 			} catch (UnknownHostException e) {
@@ -28,6 +30,7 @@ public class MongoConnection extends NoSqlConnection<MongoClient> {
 			}
 		}, "mongodb");
 		defaultDB = uri.getPathSegs()[0];
+		defaultCollection = uri.getPathSegs().length > 1 ? uri.getPathSegs()[1] : null;
 		dbs = new ConcurrentHashMap<>();
 	}
 
@@ -37,6 +40,18 @@ public class MongoConnection extends NoSqlConnection<MongoClient> {
 
 	public DB db(String dbname) {
 		return dbs.computeIfAbsent(dbname, n -> client().getDB(n));
+	}
+
+	public DBCollection collection() {
+		return db().getCollection(defaultCollection);
+	}
+
+	public DBCollection collection(String collection) {
+		return db().getCollection(collection);
+	}
+
+	public DBCollection collection(String dbname, String collection) {
+		return db(dbname).getCollection(collection);
 	}
 
 	@Override
