98adc4ef64a2aa388cac9be5ee9c322ad741c13c
कलविङ्क <zhangx@hzcominfo.com>
2017-02-09 16:28:53 +0800

    catch EsRejectedExecutionException

--- es-io/src/main/java/net/butfly/albatis/elastic/EsOutput.java
+++ es-io/src/main/java/net/butfly/albatis/elastic/EsOutput.java
@@ -3,15 +3,19 @@ package net.butfly.albatis.elastic;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.elasticsearch.action.ActionFuture;
 import org.elasticsearch.action.bulk.BulkItemResponse;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.common.util.concurrent.EsRejectedExecutionException;
 
 import net.butfly.albacore.io.faliover.FailoverOutput;
+import net.butfly.albacore.utils.async.Concurrents;
 import scala.Tuple2;
 
 public final class EsOutput extends FailoverOutput<ElasticMessage, ElasticMessage> {
@@ -40,15 +44,20 @@ public final class EsOutput extends FailoverOutput<ElasticMessage, ElasticMessag
 			if (v.isEmpty()) return 0;
 			BulkRequest req = new BulkRequest().add(v.toArray(new UpdateRequest[v.size()]));
 			logger().trace("Bulk size: " + req.estimatedSizeInBytes());
-			ActionFuture<BulkResponse> resp = conn.client().bulk(req);
-			int c = 0;
-			for (BulkItemResponse r : resp.actionGet())
-				if (!r.isFailed()) c++;
-				else throw new RuntimeException(r.getFailureMessage() + "@" + r.getFailure().getId(), r.getFailure().getCause());
-			return c;
+			do {
+				try {
+					ActionFuture<BulkResponse> resp = conn.client().bulk(req);
+					Map<Boolean, List<BulkItemResponse>> resps = StreamSupport.stream(resp.actionGet().spliterator(), true).collect(
+							Collectors.partitioningBy(r -> r.isFailed()));
+					if (!resps.get(Boolean.TRUE).isEmpty()) throw resps.get(Boolean.TRUE).get(0).getFailure().getCause();
+					else return resps.get(Boolean.FALSE).size();
+				} catch (EsRejectedExecutionException e) {
+					Concurrents.waitSleep(100);
+				}
+			} while (true);
 		} catch (RuntimeException e) {
 			throw e;
-		} catch (Exception e) {
+		} catch (Throwable e) {
 			throw new RuntimeException(e);
 		}
 	}
