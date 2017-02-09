a78bce65f6675ef648ce508ce4f4415494e3adbf
कलविङ्क <zhangx@hzcominfo.com>
2017-02-10 02:44:56 +0800

    fix bug in conv

--- kafka-io/src/main/java/net/butfly/albatis/kafka/ZKConn.java
+++ kafka-io/src/main/java/net/butfly/albatis/kafka/ZKConn.java
@@ -1,6 +1,7 @@
 package net.butfly.albatis.kafka;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -12,6 +13,7 @@ import java.util.stream.Stream;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
 
+import kafka.api.PartitionOffsetRequestInfo;
 import kafka.utils.ZkUtils;
 import net.butfly.albacore.serder.JsonSerder;
 import net.butfly.albacore.utils.logger.Logger;
@@ -42,7 +44,8 @@ public class ZKConn implements AutoCloseable {
 
 	private String fetchText(String path) {
 		try {
-			return new String(zk.getData(path, false, null));
+			byte[] b = zk.getData(path, false, null);
+			return null == b ? null : new String(b);
 		} catch (KeeperException e) {
 			logger.error("ZK failure", e);
 			return null;
@@ -52,60 +55,48 @@ public class ZKConn implements AutoCloseable {
 		}
 	}
 
-	private Map<String, Object> fetchJSON(String path) {
+	private Map<String, Object> fetch(String path) {
 		String text = fetchText(path);
 		return null == text ? null : JsonSerder.JSON_MAPPER.der(text);
 	}
 
 	public String[] getBorkers() throws IOException {
 		return fetchChildren(ZkUtils.BrokerIdsPath()).stream().map(brokenId -> {
-			Map<String, Object> info = fetchJSON(ZkUtils.BrokerIdsPath() + "/" + brokenId);
+			Map<String, Object> info = fetch(ZkUtils.BrokerIdsPath() + "/" + brokenId);
 			return info.get("host") + ":" + info.get("port");
 		}).collect(Collectors.toList()).toArray(new String[0]);
 	}
 
 	public Map<String, int[]> getTopicPartitions() {
 		@SuppressWarnings("unchecked")
-		Stream<Tuple2<String, Set<Integer>>> s = fetchChildren(ZkUtils.BrokerTopicsPath()).stream().map(
-				topic -> new Tuple2<String, Set<Integer>>(topic, ((Map<Integer, int[]>) fetchJSON(ZkUtils.getTopicPath(topic)).get(
-						"partitions")).keySet()));
-		return s.collect(Collectors.toMap(t -> t._1, t -> t._2.stream().mapToInt(i -> i).sorted().toArray()));
+		Stream<Tuple2<String, Set<Integer>>> s = fetchChildren(ZkUtils.BrokerTopicsPath()).stream().map(topic -> {
+			Tuple2<String, Set<Integer>> t = new Tuple2<String, Set<Integer>>(topic, ((Map<String, List<Integer>>) fetch(ZkUtils
+					.getTopicPath(topic)).get("partitions")).keySet().stream().map(i -> Integer.parseInt(i)).collect(Collectors.toSet()));
+			return t;
+		});
+		return s.collect(Collectors.toMap(t -> {
+			return t._1;
+		}, t -> {
+			IntStream ss = t._2.stream().mapToInt(i -> {
+				return null == i ? 0 : i.intValue();
+			});
+			return ss.sorted().toArray();
+		}));
 	}
 
 	public Map<String, int[]> getTopicPartitions(String... topics) {
 		if (topics == null || topics.length == 0) return getTopicPartitions();
 		return Arrays.asList(topics).stream().collect(Collectors.toMap(t -> t, t -> {
-			Map<String, Object> info = fetchJSON(ZkUtils.getTopicPath(t));
+			Map<String, Object> info = fetch(ZkUtils.getTopicPath(t));
 			if (null == info) return new int[0];
 			else {
-				// ZkUtils.apply("hzga136:2181,hzga137:2181,hzga138:2181/kafka",
-				// 500, 500, false).getConsumerPartitionOwnerPath(
-				// "HbaseFromKafkaTest_1", "HZGA_GAZHK_ZZRK", 0);
-				// List<String> parts = getChildren(zk,
-				// "/brokers/topics/HZGA_GAZHK_LGY_NB/partitions/0/state");
-				// Integer.parseInt(new String(new
-				// ZKConn("hzga136:2181,hzga137:2181,hzga138:2181/kafka").zk.getData(
-				// "/consumers/HbaseFromKafkaTest_1/offsets/HZGA_GAZHK_LGY_NB/0",
-				// false, null)));
-				// PartitionOffsetRequestInfo poreq = new
-				// PartitionOffsetRequestInfo(kafka.api.OffsetRequest.LatestTime(),
-				// 9);
-				// SimpleConsumer consumer = new SimpleConsumer(host, port,
-				// soTimeout, bufferSize, clientId);
-				// // consumer.getOffsetsBefore(request)
-				// OffsetRequest oreq = new OffsetRequest();
-				//
-				// getChildren(new
-				// ZKConn("hzga136:2181,hzga137:2181,hzga138:2181/kafka"),
-				// "/admin");
-				// new String(new
-				// ZKConn("hzga136:2181,hzga137:2181,hzga138:2181/kafka").zk.getData("/admin",
-				// false, null));
-
 				@SuppressWarnings("unchecked")
-				Map<Integer, int[]> counts = (Map<Integer, int[]>) info.get("partitions");
+				Map<String, List<Integer>> counts = (Map<String, List<Integer>>) info.get("partitions");
 				logger.debug(() -> "Kafka topic [" + t + "] info fetch from zk [" + zk + "]: " + counts.toString());
-				return counts.keySet().stream().mapToInt(i -> i).sorted().toArray();
+				new ArrayList<>(counts.keySet()).get(0);
+				return counts.keySet().stream().mapToInt(i -> {
+					return Integer.parseInt(i);
+				}).sorted().toArray();
 			}
 		}));
 	}
@@ -136,9 +127,22 @@ public class ZKConn implements AutoCloseable {
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, KeeperException, InterruptedException {
 		try (ZKConn zk = new ZKConn("hzga136:2181,hzga137:2181,hzga138:2181/kafka")) {
 			System.out.println(zk.getTopicPartitions());
+			String topic = "HZGA_GAZHK_LGY_NB";
+			System.out.println(ZkUtils.apply("hzga136:2181,hzga137:2181,hzga138:2181/kafka", 500, 500, false).getConsumerPartitionOwnerPath(
+					"HbaseFromKafkaTest_1", "HZGA_GAZHK_ZZRK", 0));
+			System.out.println(zk.fetch("/brokers/topics/" + topic + "/partitions/0/state"));
+			System.out.println(Integer.parseInt(zk.fetchText("/consumers/HbaseFromKafkaTest_1/offsets/" + topic + "/0")));
+			System.out.println(zk.getTopicPartitions(topic));
+			System.out.println(zk.getOffset(topic, "HbaseFromKafkaTest_1"));
+
+			PartitionOffsetRequestInfo poreq = new PartitionOffsetRequestInfo(kafka.api.OffsetRequest.LatestTime(), 9);
+			// SimpleConsumer consumer = new SimpleConsumer(host, port,
+			// soTimeout, bufferSize, clientId);
+			// consumer.getOffsetsBefore(request)
+			// OffsetRequest oreq = new OffsetRequest();
 		}
 	}
 }
