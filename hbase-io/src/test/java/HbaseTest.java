92d969b4cba9fe607c86033b34f4bb8ca054f4e5
कलविङ्क <zhangx@hzcominfo.com>
2017-02-24 21:29:49 +0800

    testing for canc

--- hbase-io/src/test/java/HbaseTest.java
+++ hbase-io/src/test/java/HbaseTest.java
@@ -1,4 +1,15 @@
 
 public class HbaseTest {
-	public static void main(String[] args) {}
+	public static void main(String[] args) {
+		System.out.println(batch(1, 1000));
+		System.out.println(batch(998, 1000));
+		System.out.println(batch(999, 1000));
+		System.out.println(batch(1000, 1000));
+		System.out.println(batch(1001, 1000));
+	}
+
+	private static int batch(int total, int batch) {
+		// return (total - 1) / batch + 1;
+		return (total + 1) / batch;
+	}
 }
