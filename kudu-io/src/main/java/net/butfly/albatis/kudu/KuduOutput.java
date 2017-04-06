ecb52329e53d7dfe8fb4cec36ccd07b227a0b892
कलविङ्क <zhangx@hzcominfo.com>
2017-04-06 18:43:41 +0800

    adaptor failover writing

--- kudu-io/src/main/java/net/butfly/albatis/kudu/KuduOutput.java
+++ kudu-io/src/main/java/net/butfly/albatis/kudu/KuduOutput.java
@@ -3,9 +3,11 @@ package net.butfly.albatis.kudu;
 import static com.hzcominfo.albatis.nosql.Connection.PARAM_KEY_BATCH;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 import org.apache.kudu.Schema;
@@ -19,29 +21,28 @@ import net.butfly.albacore.io.faliover.FailoverOutput;
 import net.butfly.albacore.io.utils.URISpec;
 
 public class KuduOutput extends FailoverOutput<String, KuduResult> {
-	
+
 	private final KuduConnection connect;
 	private final KuduSession session;
 	private final KuduTable kuduTable;
 
 	public KuduOutput(String name, URISpec uri, String failoverPath) throws IOException {
-		
-		super(name, b -> new KuduResult(b), failoverPath,
-				null == uri ? 200 : Integer.parseInt(uri.getParameter(PARAM_KEY_BATCH, "200")));
+
+		super(name, b -> new KuduResult(b), failoverPath, null == uri ? 200 : Integer.parseInt(uri.getParameter(PARAM_KEY_BATCH, "200")));
 		this.connect = new KuduConnection(uri, null);
 		this.session = connect.newSession();
 		this.kuduTable = connect.kuduTable(name);
 	}
 
 	@Override
-	protected void closeInternal() {		
-		connect.close();		
+	protected void closeInternal() {
+		connect.close();
 	}
 
 	@Override
-	protected long write(String table, Stream<KuduResult> pkg, Set<KuduResult> fails) {
-		// TODO Auto-generated method stub
-		AtomicLong c = new AtomicLong(0);		
+	protected long write(String table, Stream<KuduResult> pkg, Consumer<Collection<KuduResult>> failing, Consumer<Long> committing,
+			int retry) {
+		AtomicLong c = new AtomicLong(0);
 		Schema schema = kuduTable.getSchema();
 		pkg.parallel().forEach(r -> {
 			Map<String, Object> record = r.forWrite();
@@ -52,12 +53,12 @@ public class KuduOutput extends FailoverOutput<String, KuduResult> {
 			try {
 				OperationResponse rr = session.apply(upsert);
 				if (rr.hasRowError()) {
-					fails.add(r);
+					failing.accept(Arrays.asList(r));
 				} else {
 					c.incrementAndGet();
 				}
 			} catch (KuduException ex) {
-				fails.add(r);
+				failing.accept(Arrays.asList(r));
 			}
 
 		});
