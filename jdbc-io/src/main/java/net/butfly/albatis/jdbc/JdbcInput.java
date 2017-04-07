8e130cb9bde722e969b8fc8e64710ba7567f0f6c
कलविङ्क <zhangx@hzcominfo.com>
2017-04-07 22:56:59 +0800

    jdbc input should step ahead on dequeue

--- jdbc-io/src/main/java/net/butfly/albatis/jdbc/JdbcInput.java
+++ jdbc-io/src/main/java/net/butfly/albatis/jdbc/JdbcInput.java
@@ -5,17 +5,24 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import net.butfly.albacore.io.InputImpl;
-import net.butfly.albacore.utils.logger.Logger;
 
-public class JdbcInput extends InputImpl<ResultSet> {
-	private static final Logger logger = Logger.getLogger(JdbcInput.class);
+public class JdbcInput extends InputImpl<Map<String, Object>> {
 	private final Connection conn;
 	private final PreparedStatement stat;
+
 	private ResultSet rs;
-	private boolean rsNext;
+
+	// resultset infomation
+	private boolean next;
+	private ResultSetMetaData meta;
+	private int colCount;
+	private String[] colNames;
 
 	public JdbcInput(final String name, String jdbc, final String sql, final Object... params) throws IOException, SQLException {
 		this(name, DriverManager.getConnection(jdbc), sql, params);
@@ -23,28 +30,44 @@ public class JdbcInput extends InputImpl<ResultSet> {
 
 	public JdbcInput(final String name, Connection conn, final String sql, final Object... params) throws IOException, SQLException {
 		super(name);
-		logger.info("[" + name + "] from [" + conn.getClientInfo() + "], sql: \n\t" + sql + "]");
+		logger().info("[" + name + "] from [" + conn.getClientInfo() + "], sql: \n\t" + sql + "]");
 		this.conn = conn;
 		// this.conn.setAutoCommit(true);
 		this.conn.setTransactionIsolation(Connection.TRANSACTION_READ_UNCOMMITTED);
-		logger.debug("[" + name + "] query begin...");
+		logger().debug("[" + name + "] query begin...");
 		stat = conn.prepareStatement(sql);
 		stat.setFetchDirection(ResultSet.FETCH_FORWARD);
 
 		int i = 1;
 		for (Object p : params)
 			stat.setObject(i++, p);
-		opening(() -> {
-			try {
-				rs = stat.executeQuery();
-				rsNext = rs.next();
-			} catch (SQLException e) {
-				logger().error("SQL Error", e);
-				rs = null;
-				rsNext = false;
-			}
-		});
+		opening(this::openJdbc);
 		closing(this::closeJdbc);
+	}
+
+	private void parseMeta() throws SQLException {
+		meta = rs.getMetaData();
+		colCount = meta.getColumnCount();
+		colNames = new String[colCount];
+		for (int i = 0; i < colCount; i++) {
+			int j = i + 1;
+			colNames[i] = meta.getColumnLabel(j);
+			int ctype = meta.getColumnType(j);
+			logger().debug("ResultSet col[" + j + ":" + colNames[i] + "]: " + meta.getColumnClassName(j) + "/" + meta.getColumnTypeName(j)
+					+ "[" + ctype + "]");
+		}
+	}
+
+	private void openJdbc() {
+		try {
+			rs = stat.executeQuery();
+			next = rs.next();
+			parseMeta();
+		} catch (SQLException e) {
+			logger().error("SQL Error", e);
+			rs = null;
+			next = false;
+		}
 	}
 
 	private void closeJdbc() {
@@ -61,12 +84,31 @@ public class JdbcInput extends InputImpl<ResultSet> {
 
 	@Override
 	public boolean empty() {
-		return !rsNext;
+		return !next;
 	}
 
 	@Override
-	protected ResultSet dequeue() {
-		return rs;
+	protected Map<String, Object> dequeue() {
+		if (!next) return null;
+		Map<String, Object> r = new HashMap<>();
+		try {
+			for (int i = 0; i < colCount; i++) {
+				Object v;
+				try {
+					v = rs.getObject(i + 1);
+				} catch (SQLException e) {
+					v = null;
+				}
+				r.put(colNames[i], v);
+			}
+			return r;
+		} finally {
+			try {
+				next = rs.next();
+			} catch (SQLException e) {
+				next = false;
+			}
+		}
 	}
 
 	public JdbcInput batch(int batching) {
