0afa1f954b69a84c298b1807c9711da440bdd9c2
jiangwei <jiangw@hzcominfo.com>
2017-04-11 17:45:54 +0800

    fixed KuduOutput bug and add async client.

---
+++ kudu-io/src/main/java/net/butfly/albatis/kudu/AsyncKuduConnection.java
@@ -0,0 +1,58 @@
+package net.butfly.albatis.kudu;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.kudu.client.AsyncKuduClient;
+import org.apache.kudu.client.AsyncKuduSession;
+import org.apache.kudu.client.KuduException;
+import org.apache.kudu.client.KuduTable;
+
+import com.hzcominfo.albatis.nosql.NoSqlConnection;
+import com.stumbleupon.async.Deferred;
+
+import net.butfly.albacore.io.utils.URISpec;
+import net.butfly.albacore.utils.logger.Logger;
+
+/**
+ * @Author Naturn
+ *
+ * @Date 2017年4月10日-下午4:47:00
+ *
+ * @Version 1.0.0
+ *
+ * @Email juddersky@gmail.com
+ */
+
+public class AsyncKuduConnection extends NoSqlConnection<AsyncKuduClient> {
+
+	private static final Logger logger = Logger.getLogger(AsyncKuduConnection.class);
+
+	protected AsyncKuduConnection(URISpec kuduUri, Map<String, String> props) throws IOException {
+		super(kuduUri, r -> {
+				return new AsyncKuduClient.AsyncKuduClientBuilder(kuduUri.getHost()).build();			
+		}, "Async", "kudu");
+	}
+
+	public AsyncKuduConnection(String kuduUri, Map<String, String> props) throws IOException {
+		this(new URISpec(kuduUri), props);
+	}
+	
+	@Override
+	public void close() {
+		try {
+			super.close();
+			client().close();
+		} catch (Exception e) {
+			logger.error("Close failure", e);
+		}
+	}
+
+	public Deferred<KuduTable> kuduTable(String table) throws KuduException {
+		return super.client().openTable(table);
+	}
+
+	public AsyncKuduSession newSession() {
+		return super.client().newSession();
+	}
+}
--- kudu-io/src/main/java/net/butfly/albatis/kudu/KuduOutput.java
+++ kudu-io/src/main/java/net/butfly/albatis/kudu/KuduOutput.java
@@ -3,9 +3,12 @@ package net.butfly.albatis.kudu;
 import static com.hzcominfo.albatis.nosql.Connection.PARAM_KEY_BATCH;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
@@ -15,7 +18,6 @@ import org.apache.kudu.client.KuduClient;
 import org.apache.kudu.client.KuduException;
 import org.apache.kudu.client.KuduSession;
 import org.apache.kudu.client.KuduTable;
-import org.apache.kudu.client.OperationResponse;
 import org.apache.kudu.client.SessionConfiguration.FlushMode;
 import org.apache.kudu.client.Upsert;
 
@@ -30,11 +32,14 @@ public class KuduOutput extends FailoverOutput<String, KuduResult> {
 
 	public KuduOutput(String name, URISpec uri, String failoverPath) throws IOException {
 
-		super(name, b -> new KuduResult(b), failoverPath, null == uri ? 200 : Integer.parseInt(uri.getParameter(PARAM_KEY_BATCH, "200")));
+		super(name, b -> new KuduResult(b), failoverPath,
+				null == uri ? 200 : Integer.parseInt(uri.getParameter(PARAM_KEY_BATCH, "200")));
 		this.connect = new KuduConnection(uri, null);
 		this.session = connect.newSession();
 		this.session.setFlushMode(FlushMode.AUTO_FLUSH_BACKGROUND);
+		this.session.setTimeoutMillis(10000);
 		open();
+		failHandler();
 	}
 
 	@Override
@@ -43,44 +48,76 @@ public class KuduOutput extends FailoverOutput<String, KuduResult> {
 			session.flush();
 		} catch (KuduException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException("kudu flush faile.");
+		}finally {
+			connect.close();
 		}
-		connect.close();
 	}
 
 	@Override
-	protected long write(String table, Stream<KuduResult> pkg, Consumer<Collection<KuduResult>> failing, Consumer<Long> committing,
-			int retry) {
+	protected long write(String table, Stream<KuduResult> pkg, Consumer<Collection<KuduResult>> failing,
+			Consumer<Long> committing, int retry) {
 		try {
 			this.kuduTable = connect.kuduTable(name);
 		} catch (KuduException e) {
 			// TODO Auto-generated catch block
-			throw new RuntimeException("connect kudu table faile.");
+			throw new RuntimeException("connect kudu table faile."+e.getMessage());
 		}
 		AtomicLong c = new AtomicLong(0);
 		Schema schema = kuduTable.getSchema();
+		List<String> keys = new ArrayList<>();
+		schema.getPrimaryKeyColumns().forEach(p->{
+			keys.add(p.getName());
+		});
 		pkg.parallel().forEach(r -> {
 			Map<String, Object> record = r.forWrite();
-			Upsert upsert = kuduTable.newUpsert();
-			schema.getColumns().forEach(p -> {
-				KuduCommon.generateColumnData(p.getType(), upsert.getRow(), p.getName(), record.get(p.getName()));
-			});
-			try {
-				OperationResponse rr = session.apply(upsert);
-				if (rr.hasRowError()) {
-					failing.accept(Arrays.asList(r));
-				} else {
+			if(record.keySet().containsAll(keys)){
+				Upsert upsert = kuduTable.newUpsert();
+				schema.getColumns().forEach(p -> {
+					if (record.containsKey(p.getName()))
+						KuduCommon.generateColumnData(p.getType(), upsert.getRow(), p.getName(), record.get(p.getName()));
+				});
+				try {
+					session.apply(upsert);
 					c.incrementAndGet();
+				} catch (KuduException ex) {
+					failing.accept(Arrays.asList(r));
 				}
-			} catch (KuduException ex) {
-				failing.accept(Arrays.asList(r));
-			}
-
+			}			
 		});
 		return c.get();
 	}
-	
-	public KuduClient client(){		
+
+	public KuduClient client() {
 		return connect.client();
+	}
+
+	public void failHandler() {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				for (;;) {
+					if (session.getPendingErrors() != null) {
+						Arrays.asList(session.getPendingErrors().getRowErrors()).forEach(p -> {
+							try {
+								session.apply(p.getOperation());
+							} catch (KuduException e) {
+								// TODO Auto-generated catch block
+								throw new RuntimeException("retry apply operation fail.");
+							}
+						});
+					}else{
+						try {
+							TimeUnit.SECONDS.sleep(1);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							throw new RuntimeException("retry time waitting exception.");
+						}
+					}
+				}
+			}
+		}).start();
+
 	}
 }
