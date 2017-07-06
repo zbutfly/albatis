eb590c86d77275ded96bc1b2b1db31deb12f3bc0
कलविङ्क <zhangx@hzcominfo.com>
2017-07-06 14:02:45 +0800

    add solr input message class for refactoring

--- solr-io/src/main/java/net/butfly/albatis/solr/SolrMessage.java
+++ solr-io/src/main/java/net/butfly/albatis/solr/SolrMessage.java
@@ -1,6 +1,7 @@
 package net.butfly.albatis.solr;
 
 import org.apache.solr.common.SolrDocumentBase;
+import org.apache.solr.common.SolrInputDocument;
 
 import net.butfly.albacore.io.Message;
 import net.butfly.albacore.utils.Systems;
@@ -43,5 +44,18 @@ public class SolrMessage<D extends SolrDocumentBase<?, D>> extends Message<Strin
 	@Override
 	public String toString() {
 		return core + ":\n\t" + doc.toString();
+	}
+
+	public static final class SolrInputMessage extends SolrMessage<SolrInputDocument> {
+		private static final long serialVersionUID = -1677835898716156514L;
+
+		public SolrInputMessage(String core, SolrInputDocument doc, boolean delete) {
+			super(core, doc, delete);
+		}
+
+		public SolrInputMessage(String core, SolrInputDocument doc) {
+			super(core, doc);
+		}
+
 	}
 }
