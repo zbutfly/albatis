e14657cd32bd0f3444db9c60ca6995b51d68a6a5
कलविङ्क <zhangx@hzcominfo.com>
2017-09-13 14:55:43 +0800

    change failover output wrapper closing order check es output empty bulk req check es output empty fail resp

--- core/src/main/java/net/butfly/albatis/io/ext/FailoverOutput.java
+++ core/src/main/java/net/butfly/albatis/io/ext/FailoverOutput.java
@@ -1,14 +1,19 @@
 package net.butfly.albatis.io.ext;
 
-import net.butfly.albacore.paral.Sdream;
-import net.butfly.albacore.paral.Task;
+import static net.butfly.albacore.utils.collection.Streams.of;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Stream;
+
+import net.butfly.albacore.io.EnqueueException;
 import net.butfly.albacore.utils.OpenableThread;
+import net.butfly.albacore.utils.parallel.Concurrents;
 import net.butfly.albatis.io.Output;
 import net.butfly.albatis.io.Queue;
 import net.butfly.albatis.io.Wrapper;
 
 /**
- * Output with buffer and pool supporting.<br>
+ * Output with buffer and fails supporting.<br>
  * Parent class handle buffer, invoking really write/marshall op by callback provided by children classes.<br>
  * Children classes define and implemented connection to datasource.
  * 
@@ -17,47 +22,47 @@ import net.butfly.albatis.io.Wrapper;
  * @param <M>
  */
 public class FailoverOutput<M> extends Wrapper.WrapOutput<M, M> {
-	protected final Queue<M> pool;
+	protected final Queue<M> fails;
 	protected final OpenableThread failovering;
+	protected final AtomicLong actionCount = new AtomicLong(0);
 
-	public FailoverOutput(Output<M> output, Queue<M> failover) {
+	public FailoverOutput(Output<M> output, Queue<M> failover, int batchSize) {
 		super(output, "Failover");
-		this.pool = failover;
+		this.fails = failover;
 		failovering = new OpenableThread(() -> {
-			while (output.opened() && opened()) {
-				while (failover.empty())
-					if (!output.opened() || !opened() || !Task.waitSleep()) break;
-				failover.dequeue(output::enqueue);
-			}
-			logger().info("Failovering stopped.");
-		}, name() + "Failovering");
+			while (opened())
+				failover.dequeue(ms -> output.enqueue(ms), batchSize);
+		}, "Failovering");
+		opening(() -> {
+			fails.open();
+			failovering.open();
+		});
 		closing(() -> {
 			failovering.close();
-			if (!pool.empty()) logger().warn("Failovering pool not empty [" + pool.size() + "], maybe lost.");
-			else logger().info("Failovering pool empty and closing.");
-			pool.close();
+			fails.close();
+			long act;
+			while ((act = actionCount.longValue()) > 0 && Concurrents.waitSleep())
+				logger().info("Failover Async op waiting for closing: " + act);
 		});
-		pool.open();
 	}
 
 	@Override
-	public void open() {
-		super.open();
-		super.base.open();
-	}
-
-	@Override
-	public final void enqueue(Sdream<M> els) {
-		base.enqueue(els);
-	}
-
-	@Override
-	public void failed(Sdream<M> failed) {
-		pool.enqueue(failed);
+	public final long enqueue(Stream<M> els) {
+		try {
+			return base.enqueue(els);
+		} catch (EnqueueException ex) {
+			fails.enqueue(of(ex.fails()));
+			return ex.success();
+		}
 	}
 
 	public final long fails() {
-		return pool.size();
+		return fails.size();
+	}
+
+	@Override
+	public void close() {
+		super.close();
 	}
 
 	@Override
--- es-io/src/main/java/net/butfly/albatis/elastic/ElasticOutput.java
+++ es-io/src/main/java/net/butfly/albatis/elastic/ElasticOutput.java
@@ -1,121 +1,153 @@
 package net.butfly.albatis.elastic;
 
-import static net.butfly.albacore.paral.Sdream.of;
 import static net.butfly.albacore.utils.Exceptions.unwrap;
+import static net.butfly.albacore.utils.collection.Streams.collect;
+import static net.butfly.albacore.utils.collection.Streams.list;
+import static net.butfly.albacore.utils.collection.Streams.of;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
+import java.util.Set;
+import java.util.concurrent.ConcurrentMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.DocWriteRequest;
 import org.elasticsearch.action.bulk.BulkItemResponse;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.common.util.concurrent.EsRejectedExecutionException;
+import org.elasticsearch.index.engine.VersionConflictEngineException;
 import org.elasticsearch.index.mapper.MapperException;
 import org.elasticsearch.transport.RemoteTransportException;
 
-import net.butfly.albacore.paral.Sdream;
-import net.butfly.albacore.utils.collection.Colls;
-import net.butfly.albacore.utils.collection.Maps;
-import net.butfly.albacore.utils.logger.Logger;
-import net.butfly.albacore.utils.logger.Statistic;
+import net.butfly.albacore.base.Namedly;
+import net.butfly.albacore.io.EnqueueException;
+import net.butfly.albacore.utils.Exceptions;
+import net.butfly.albacore.utils.Texts;
 import net.butfly.albatis.io.Message;
-import net.butfly.albatis.io.OutputBase;
+import net.butfly.albatis.io.Output;
 
-public class ElasticOutput extends OutputBase<Message> {
-	static final Logger logger = Logger.getLogger(ElasticOutput.class);
-	public static final int MAX_RETRY = 3;
+public final class ElasticOutput extends Namedly implements Output<Message> {
+	public static final int SUGGEST_RETRY = 5;
 	public static final int SUGGEST_BATCH_SIZE = 1000;
-	protected final ElasticConnection conn;
+	private final ElasticConnection conn;
+	private final int maxRetry;
 
 	public ElasticOutput(String name, ElasticConnection conn) throws IOException {
 		super(name);
 		this.conn = conn;
-	}
-
-	@Override
-	public Statistic trace() {
-		return new Statistic(this).sizing(BulkRequest::estimatedSizeInBytes)//
-				.<BulkRequest> batchSizeCalcing(r -> (long) r.requests().size())//
-				.<BulkRequest> sampling(r -> r.requests().isEmpty() ? null : r.requests().get(0).toString());
+		maxRetry = SUGGEST_RETRY;
+		open();
 	}
 
 	@Override
 	public void close() {
-		super.close();
 		try {
 			conn.close();
 		} catch (Exception e) {}
 	}
 
 	@Override
-	protected void enqueue0(Sdream<Message> msgs) {
-		Map<Object, Message> remains = Maps.of();
-		for (Message m : msgs.list())
-			remains.put(m.key(), conn.fixTable(m));
-		if (!remains.isEmpty()) go(remains);
-	}
-
-	protected void go(Map<Object, Message> remains) {
-		// logger.error("INFO: es op task enter with [" + ops.get() + "] pendings.");
+	public final long enqueue(Stream<Message> msgs) throws EnqueueException {
+		ConcurrentMap<String, Message> origin = msgs.collect(Collectors.toConcurrentMap(m -> m.key(), m -> m));
+		if (origin.isEmpty()) return 0;
+		int originSize = origin.size();
 		int retry = 0;
-		// int size = remains.size();
-		try {
-			while (!remains.isEmpty() && retry++ <= MAX_RETRY) {
-				@SuppressWarnings("rawtypes")
-				List<DocWriteRequest> reqs = of(remains.values()).map(Elastics::forWrite).list();
-				if (reqs.isEmpty()) return;
-				BulkRequest bulk = new BulkRequest().add(reqs);
-				process(remains, s().statsOut(bulk, b -> {
-					// logger().error("Elastic step [" + size + "] begin.");
-					// long now = System.currentTimeMillis();
-					try {
-						return conn.client().bulk(b).get();
-					} catch (ExecutionException ex) {
-						logger().error("Elastic client fail: [" + ex.getCause() + "]");
-					} catch (Exception ex) {
-						logger().error("Elastic client fail: [" + ex + "]");
-						// } finally {
-						// logger().error("Elastic step [" + size + "] spent: " + (System.currentTimeMillis() - now) + " ms.");
+		EnqueueException eex = new EnqueueException();
+		while (!origin.isEmpty() && retry++ <= maxRetry) {
+			@SuppressWarnings("rawtypes")
+			List<DocWriteRequest> reqs = list(origin.values(), Elastics::forWrite);
+			if (reqs.isEmpty()) return 0;
+			BulkRequest req = new BulkRequest().add(reqs);
+			long bytes = logger().isTraceEnabled() ? req.estimatedSizeInBytes() : 0;
+			long now = System.currentTimeMillis();
+			int currentRetry = retry;
+			try {
+				conn.client().bulk(req, new ActionListener<BulkResponse>() {
+					@Override
+					public void onResponse(BulkResponse response) {
+						Map<Boolean, List<BulkItemResponse>> resps = collect(response, Collectors.partitioningBy(r -> r.isFailed()));
+						List<BulkItemResponse> succResps = resps.get(Boolean.FALSE);
+						List<BulkItemResponse> failResps = resps.get(Boolean.TRUE);
+						String sample = succResps.isEmpty() ? null : succResps.get(0).getId();
+						Result result = null;
+						eex.success(succResps.size());
+						if (failResps.isEmpty()) {//
+							origin.clear();
+							result = new Result(succResps.size(), 0, sample);
+						} else {
+							// process success: remove from origin
+							succResps.forEach(succ -> origin.remove(succ.getId()));
+							// process failing and retry...
+							Map<Boolean, List<BulkItemResponse>> failOrRetry = of(failResps).collect(Collectors.partitioningBy(
+									ElasticOutput.this::failed));
+							failOrRetry.get(Boolean.TRUE).forEach(r -> {
+								Message m = origin.remove(r.getId());
+								Exception cause = r.getFailure().getCause();
+								if (null != m) eex.fail(m, cause);
+								else logger().debug("Message [" + r.getId() + "] could not failover for [" + cause.getMessage()
+										+ "], maybe processed or lost");
+							});
+							Set<String> failIds = collect(failOrRetry.get(Boolean.TRUE), r -> r.getFailure().getId(), Collectors.toSet());
+							if (logger().isDebugEnabled()) //
+								failOrRetry.get(Boolean.TRUE).forEach(r -> logger().warn("Writing failed id [" + r.getId() + "] for: "
+										+ unwrap(r.getFailure().getCause()).getMessage()));
+
+							// process and stats success...
+							result = new Result(succResps.size(), failIds.size(), sample);
+						}
+						if (result != null && logger().isTraceEnabled()) result.trace(origin.size(), bytes, currentRetry, origin.size(),
+								System.currentTimeMillis() - now);
 					}
-					return null;
-				}));
+
+					@Override
+					public void onFailure(Exception e) {
+						Throwable t = Exceptions.unwrap(e);
+						logger().warn("Elastic connection op failed [" + t + "], [" + origin.size() + "] fails", t);
+						eex.fails(origin.values());
+					}
+				});
+			} catch (IllegalStateException ex) {
+				logger().error("Elastic client fail: [" + ex.getMessage() + "]");
+				eex.fails(origin.values());
 			}
-		} finally {
-			if (!remains.isEmpty()) failed(of(remains.values()));
+		}
+		if (eex.empty()) return originSize;
+		else throw eex;
+	}
+
+	private final class Result {
+		final long succs;
+		final long fails;
+		final String sample;
+
+		Result(long succs, long fails, String sample) {
+			super();
+			this.succs = succs;
+			this.fails = fails;
+			this.sample = sample;
+		}
+
+		void trace(long total, long bytes, int retry, int remains, long ms) {
+			StringBuilder sb = new StringBuilder()//
+					.append("Try#").append(retry).append(" finished, total:[").append(total).append("/")//
+					.append(Texts.formatKilo(bytes, " bytes")).append("] in [").append(ms)//
+					.append("] ms, step successed:[" + succs + "], ");
+			if (remains > 0) sb.append("remained:[").append(remains).append("], ");
+			if (fails > 0) sb.append("failed:[").append(fails).append("], ");
+			logger().trace(sb.append("sample id: [").append(sample).append("]."));
 		}
 	}
 
-	void process(Map<Object, Message> remains, BulkResponse response) {
-		int succs = 0, respSize = remains.size();
-		// int originalSize = remains.size();
-		if (respSize == 0) return;
-		List<Message> retries = Colls.list();
-		if (null != response) for (BulkItemResponse r : response) {
-			Message o = remains.remove(r.getId());
-			if (!r.isFailed()) succs++;
-			else if (null != o) {
-				if (noRetry(r.getFailure().getCause())) {
-					logger.error(() -> "ElasticOutput [" + name() + "] failed for [" + unwrap(r.getFailure().getCause()).toString()
-							+ "]: \n\t" + o.toString());
-				} else retries.add(o);
-			}
-		}
-		if (succs > 0) succeeded(succs);
-		// process failing and retry...
-		if (!retries.isEmpty()) failed(of(retries));
-		// logger.error("INFO: es op resp parsed: resps [" + respSize + "], remains [" + originalSize + "=>" + remains.size() + "], "//
-		// + "success [" + succs + "], failover [" + retries.size() + "].");
-	}
-
-	boolean noRetry(Throwable cause) {
-		while (RemoteTransportException.class.isAssignableFrom(cause.getClass()) && cause.getCause() != null)
-			cause = cause.getCause();
-		if (MapperException.class.isAssignableFrom(cause.getClass())) logger().error("ES mapper exception", cause);
-		return // EsRejectedExecutionException.class.isAssignableFrom(cause.getClass()) ||
-				// VersionConflictEngineException.class.isAssignableFrom(c) ||
-		MapperException.class.isAssignableFrom(cause.getClass());
+	private boolean failed(BulkItemResponse r) {
+		Throwable cause = r.getFailure().getCause();
+		Class<? extends Throwable> c = cause.getClass();
+		if (MapperException.class.isAssignableFrom(c)) logger().error("ES mapper exception", cause);
+		return EsRejectedExecutionException.class.isAssignableFrom(c) || VersionConflictEngineException.class.isAssignableFrom(c)
+				|| MapperException.class.isAssignableFrom(c);
 	}
 
 	static {
