8ab83d2b020150cdc32d4395fa9f56acd1761c9e
कलविङ्क <zhangx@hzcominfo.com>
2017-10-22 03:18:39 +0800

    change pump batching to sys props

--- core/src/main/java/com/hzcominfo/albatis/Albatis.java
+++ core/src/main/java/com/hzcominfo/albatis/Albatis.java
@@ -3,8 +3,5 @@ package com.hzcominfo.albatis;
 public interface Albatis {
 	interface Props {
 		final String PROP_PUMP_BATCH_SIZE = "albatis.pump.batch.size";
-		final String PROP_KUDU_TABLE_BUCKETS = "albatis.kudu.table.buckets";
-		final String PROP_KUDU_TABLE_REPLICAS = "albatis.kudu.table.replicas";
-		final String PROP_KUDU_TIMEOUT = "albatis.kudu.timeout";
 	}
 }
