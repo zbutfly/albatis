8505f3260bee2fd1825fcc2a29b8537f0dcc4202
taidl <taidl@hzcominfo.com>
2017-11-16 11:11:52 +0800

    explain sql to json

--- core/src/main/java/com/hzcominfo/dataggr/uniquery/SqlExplainer.java
+++ core/src/main/java/com/hzcominfo/dataggr/uniquery/SqlExplainer.java
@@ -1,11 +1,419 @@
 package com.hzcominfo.dataggr.uniquery;
 
-import org.apache.calcite.sql.SqlNode;
+import com.google.common.collect.ImmutableList;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import org.apache.calcite.avatica.util.Casing;
+import org.apache.calcite.avatica.util.Quoting;
+import org.apache.calcite.sql.*;
+import org.apache.calcite.sql.parser.SqlParser;
+import org.apache.calcite.sql.parser.impl.SqlParserImpl;
+import org.apache.calcite.sql.util.SqlBasicVisitor;
+import org.apache.calcite.sql.validate.SqlConformanceEnum;
+import org.apache.calcite.util.NlsString;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class SqlExplainer {
 
-	 // 解析SQL
-    public static SqlNode explain(String sql) {
-    	return null;
+    public static SqlParser.ConfigBuilder DEFAULT_PARSER_CONFIG = SqlParser
+            .configBuilder()
+            .setParserFactory(SqlParserImpl.FACTORY)
+            .setQuoting(Quoting.DOUBLE_QUOTE)
+            .setUnquotedCasing(Casing.UNCHANGED)
+            .setQuotedCasing(Casing.UNCHANGED)
+            .setConformance(SqlConformanceEnum.DEFAULT)
+//                .setConformance(SqlConformanceEnum.LENIENT) // allow everything
+//            .setConformance(SqlConformanceEnum.MYSQL_5) // allow Limit start,count ,,,start is from 1
+            .setCaseSensitive(true);
+
+    public static final SqlBasicVisitor<String> visitor = new SqlBasicVisitor<String>() {
+        @Override
+        public String visit(SqlLiteral literal) {
+
+//                System.out.println("literal(文本常量)===>" + literal.toString());
+            return super.visit(literal);
+        }
+
+        @Override
+        public String visit(SqlCall call) {
+            switch (call.getKind()) {
+                case SELECT:
+                    SqlSelect select = (SqlSelect) call;
+                    System.out.println("selectList:  " + select.getSelectList().getList().stream().map(SqlNode::toString).reduce("", (s1, s2) -> s1 + "\n" + s2));
+                    System.out.println("from     " + select.getFrom());
+                    if (select.hasWhere()) {
+                        SqlNode where = select.getWhere();
+                        System.out.println("where    " + where + " " + where.getKind() + where.getClass().getSimpleName());
+                    }
+                    if (null != select.getGroup())
+                        System.out.println("groupBy  " + select.getGroup().getList().stream().filter(Objects::nonNull).map(SqlNode::toString).reduce("\n", (s1, s2) -> s1 + "\n" + s2));
+                    System.out.println("having " + select.getHaving());
+                    if (select.hasOrderBy()) System.out.println("orderBy  " + select.getOrderList());
+                    System.out.println("offset " + select.getOffset());
+
+                        /*select.getOperandList().stream().filter(Objects::nonNull).forEach(n -> {
+                            System.out.println("\nselect: " + n.getKind() + "\n" + n.toString());
+                        });*/
+                    break;
+                case AS:
+                    System.out.println("AS::   " + ((SqlBasicCall) call).getOperandList());
+                    break;
+                case AND:
+                    System.out.println("AND::   " + ((SqlBasicCall) call).getOperandList());
+                    break;
+                case CAST:
+                    System.out.println("CAST::   " + ((SqlBasicCall) call).getOperandList());
+                    break;
+                default:
+//                        System.out.println(call.getKind().name() + " ???? " + call.getClass().getName());
+
+            }
+//                    call.getOperandList().forEach(n -> System.out.println(n.toString()));
+            return super.visit(call);
+        }
+
+        @Override
+        public String visit(SqlNodeList nodeList) {
+//                nodeList.forEach(n -> System.out.println("nodeList===>" + n.getClass().getName()));
+            return super.visit(nodeList);
+        }
+
+        @Override
+        public String visit(SqlIdentifier id) {
+//                System.out.println("identifier===>" + id.getSimple());
+            return super.visit(id);
+        }
+
+        @Override
+        public String visit(SqlDataTypeSpec type) {
+
+            return super.visit(type);
+        }
+
+        @Override
+        public String visit(SqlDynamicParam param) {
+            System.out.println("param index at " + param.getIndex());
+            return super.visit(param);
+        }
+
+        @Override
+        public String visit(SqlIntervalQualifier intervalQualifier) {
+            return super.visit(intervalQualifier);
+        }
     };
+
+    private static Map<String, SqlNode> cache = new HashMap<>();
+
+    public static JsonObject parseSql(String sql, Object... params) throws Exception {
+        if (null == sql || sql.isEmpty()) return null;
+        SqlNode node = cache.get(sql);
+        if (null == node) {
+            node = SqlParser.create(sql, DEFAULT_PARSER_CONFIG.build()).parseQuery();
+            if (!node.isA(SqlKind.QUERY)) throw new Exception("Non-query expression ```" + sql + "``` is NOT supported.");
+            cache.put(sql, node);
+        }
+
+        JsonObject json = new JsonObject();
+        json.addProperty("sql", sql);
+
+        switch (node.getKind()) {
+            case SELECT:
+                SqlSelect select = (SqlSelect) node;
+                analysisSqlSelect(select, json);
+                break;
+            case ORDER_BY:
+                SqlOrderBy orderBy = (SqlOrderBy) node;
+                SqlNode query = orderBy.query;
+                analysisSqlSelect((SqlSelect) query, json);
+                SqlNode fetch = orderBy.fetch;
+                analysisLimit(fetch, json);
+                SqlNode offset = orderBy.offset;
+                analysisOffset(offset, json);
+                SqlNodeList orderList = orderBy.orderList;
+                analysisOrderBy(orderList, json);
+                break;
+            case UNION:
+            case INTERSECT:
+            case EXCEPT:
+            case VALUES:
+            case WITH:
+            case EXPLICIT_TABLE:
+            default:
+        }
+        return json;
+    }
+
+    private static void analysisSqlSelect(SqlSelect select, JsonObject json) {
+        //keywordList
+        analysisKeywords(select, json);
+        //selectList
+        analysisFields(select.getSelectList().getList(), json);
+        //from ok
+        analysisTables(select, json);
+        //where //todo not finished
+        if (select.hasWhere()) analysisConditions(select.getWhere(), json);
+        //groupBy
+        analysisGroupBy(select.getGroup(), json);
+        //having
+        analysisHaving(select.getHaving(), json);
+        //order
+        analysisOrderBy(select.getOrderList(), json);
+        // offset
+        analysisOffset(select.getOffset(), json);
+        // limit
+        analysisLimit(select.getFetch(), json);
+    }
+
+    private static void analysisKeywords(SqlSelect select, JsonObject json) {
+        json.addProperty("distinct", select.isDistinct());
+    }
+
+    private static void analysisFields(List<SqlNode> selectList, JsonObject json) {
+        JsonArray fields = new JsonArray();
+        for (int i = 0; i < selectList.size(); i++) {
+            SqlNode n = selectList.get(i);
+            SqlIdentifier identifier;
+            JsonObject field = new JsonObject();
+            switch (n.getKind()) {
+                case AS:
+                    identifier = ((SqlCall) n).operand(0);
+                    identifier2Json(identifier, field);
+                    field.addProperty("alias", ((SqlIdentifier) ((SqlCall) n).operand(1)).getSimple());
+                    break;
+                case IDENTIFIER:
+                    identifier = (SqlIdentifier) n;
+                    identifier2Json(identifier, field);
+//                    field.addProperty("field", identifier.isStar() ? "*" : identifier.getSimple());
+                    break;
+                case SUM:
+                case AVG:
+                case COUNT:
+                case MAX:
+                case MIN:
+                default:
+                    throw new RuntimeException("Unsupported kind: " + n.getKind());
+            }
+            fields.add(field);
+        }
+        json.add("fields", fields);
+    }
+
+    private static void identifier2Json(SqlIdentifier identifier, JsonObject json) {
+        ImmutableList<String> names = identifier.names.reverse();
+        if (names.size() >= 3) json.addProperty("namespace", names.get(2));
+        if (names.size() >= 2) json.addProperty("table", names.get(1));
+        if (names.size() >= 1) json.addProperty("field", identifier.isStar() ? "*" : names.get(0));
+    }
+
+    private static void analysisTables(SqlSelect select, JsonObject json) {
+        SqlNode from = select.getFrom();
+        JsonObject tree = new JsonObject();
+        analysisWithJoin(from, tree);
+        json.add("tables", tree);
+    }
+
+    private static void analysisWithJoin(SqlNode join, JsonObject tree) {
+        switch (join.getKind()) {
+            case IDENTIFIER:
+                SqlIdentifier identifier = (SqlIdentifier) join;
+                tree.addProperty("table", identifier.getSimple());
+                break;
+            case AS:
+                tree.addProperty("table", ((SqlIdentifier) ((SqlCall) join).operand(0)).getSimple());
+                tree.addProperty("alias", ((SqlIdentifier) ((SqlCall) join).operand(1)).getSimple());
+                break;
+            case JOIN:
+                JsonObject left = new JsonObject(), right = new JsonObject();
+                SqlJoin sqlJoin = (SqlJoin) join;
+                analysisWithJoin(sqlJoin.getLeft(), left);
+                analysisWithJoin(sqlJoin.getRight(), right);
+                tree.addProperty("type", sqlJoin.getJoinType().lowerName);
+                tree.add("left", left);
+                tree.add("right", right);
+                break;
+            default:
+                throw new RuntimeException("Unsupported from kind: " + join.getKind());
+        }
+    }
+
+    private static void analysisConditions(SqlNode where, JsonObject json) {
+        JsonObject tree = new JsonObject();
+        analysisConditionExpression(where, tree);
+        json.add("where", tree);
+    }
+
+    private static void analysisConditionExpression(SqlNode condition, JsonObject tree) {
+//        System.out.println("\n" + condition);
+        SqlKind kind = condition.getKind();
+        SqlCall sc = (SqlCall) condition;
+
+        switch (kind) {
+            case AND:
+            case OR:
+                JsonArray array = new JsonArray();
+                analysisCompoundConditionExpression(sc, array);
+                tree.add(kind.lowerName, array);
+                break;
+            case IS_NULL:
+            case IS_NOT_NULL:
+                analysisUnaryConditionExpression(sc, tree);
+                break;
+            case GREATER_THAN:
+            case LESS_THAN:
+            case GREATER_THAN_OR_EQUAL:
+            case LESS_THAN_OR_EQUAL:
+            case EQUALS:
+            case NOT_EQUALS:
+            case LIKE:
+//            case NOT:
+                JsonObject o = new JsonObject();
+                analysisBinaryConditionExpression(sc, o);
+                tree.add(kind.lowerName, o);
+                break;
+            case IN:
+//                JsonObject ij =
+            case NOT_IN:
+                analysisMultipleConditionExpression(sc, tree);
+//                throw new RuntimeException("Condition " + kind.lowerName + " NOT supported");
+                break;
+            default:
+                throw new RuntimeException("Unsupported Condition kind: " + kind.name());
+        }
+    }
+
+    /**
+     * 复合表达式解析
+     * @param sc
+     * @param array
+     */
+    private static void analysisCompoundConditionExpression(SqlCall sc, JsonArray array) {
+        JsonObject lj = new JsonObject(), rj = new JsonObject();
+        SqlNode ln = sc.operand(0), rn = sc.operand(1);
+        analysisConditionExpression(ln, lj);
+        analysisConditionExpression(rn, rj);
+        array.add(lj);
+        array.add(rj);
+    }
+
+    /**
+     * 一元表达式解析
+     * @param sc
+     * @param object
+     */
+    private static void analysisUnaryConditionExpression(SqlCall sc, JsonObject object) {
+        String snn = ((SqlIdentifier) sc.operand(0)).getSimple();
+        JsonObject nnj = new JsonObject();
+        nnj.add(snn, null);
+        object.add(sc.getKind() == SqlKind.IS_NULL ? SqlKind.EQUALS.lowerName : SqlKind.NOT_EQUALS.lowerName, nnj);
+    }
+
+    /**
+     * 二元表达式解析
+     * @param sc
+     * @param json
+     */
+    private static void analysisBinaryConditionExpression(SqlCall sc, JsonObject json) {
+        String field = ((SqlIdentifier) sc.operand(0)).getSimple();
+        SqlNode v = sc.operand(1);
+        if (v instanceof SqlIdentifier) {
+            throw new RuntimeException("if " + ((SqlIdentifier) v).getSimple() + " (" + v.getParserPosition().toString() + ") is literal, please mark them by single quote");
+        }
+//        Object value = ((SqlLiteral) sc.operand(1)).getValue();
+        String value = ((SqlLiteral) sc.operand(1)).toValue();
+        json.addProperty(field, value);
+    }
+
+    /**
+     * 多元表达式解析
+     * @param sc
+     * @param json
+     */
+    private static void analysisMultipleConditionExpression(SqlCall sc, JsonObject json) {
+        System.out.println(sc);
+//        String operator = sc.getOperator().getName();
+        String operator = sc.getKind().lowerName;
+        String field = ((SqlIdentifier) sc.operand(0)).getSimple();
+        SqlNode nodes = sc.operand(1);
+        JsonObject object = new JsonObject();
+        JsonArray array = new JsonArray();
+        object.add(field, array);
+        json.add(operator, object);
+        for (SqlNode node : ((SqlNodeList) nodes).getList()) {
+            if (!( node instanceof SqlLiteral)) throw new RuntimeException(node + " is NOT a literal");
+            Object value = ((SqlLiteral) node).getValue();
+            if (value instanceof BigDecimal) array.add(((BigDecimal) value).longValue());
+            else if (value instanceof NlsString) array.add(((NlsString) value).getValue());
+        }
+    }
+
+    private static void analysisGroupBy(SqlNodeList groupList, JsonObject json) {
+        JsonArray array = new JsonArray();
+        json.add("groupBy", array);
+        if (null == groupList) return;
+        groupList.getList().forEach(node -> {
+            if (node.getKind() == SqlKind.IDENTIFIER) {
+                array.add(((SqlIdentifier) node).getSimple());
+            } else {
+                System.out.println("Unsupported GROUP kind " + node.getKind().name());
+            }
+        });
+    }
+
+    private static void analysisHaving(SqlNode having, JsonObject json) {
+        JsonObject array = new JsonObject();
+        json.add("having", array);
+        if (null == having) return;
+        System.out.println(having);
+        throw new RuntimeException(having.getKind().name() + " is NOT supported");
+    }
+
+    private static void analysisOrderBy(SqlNodeList orderList, JsonObject json) {
+        JsonArray array = new JsonArray();
+        json.add("orderBy", array);
+        if (null == orderList) return;
+        for (SqlNode node : orderList.getList()) {
+            JsonObject object = new JsonObject();
+            if (node instanceof SqlIdentifier) {
+                object.addProperty(((SqlIdentifier) node).getSimple(), "ASC");
+                array.add(object);
+            } else if (node instanceof SqlBasicCall) {
+                SqlBasicCall sbc = (SqlBasicCall) node;
+                SqlIdentifier identifier = sbc.operand(0);
+                object.addProperty(identifier.getSimple(), sbc.getOperator().getName());
+                array.add(object);
+            } else {
+                System.out.println("Error to parse ORDER BY from " + node);
+            }
+        }
+    }
+
+    private static void analysisOffset(SqlNode node, JsonObject json) {
+        long offset = 0L;
+        if (node instanceof SqlNumericLiteral) {
+            offset = ((SqlNumericLiteral) node).bigDecimalValue().longValue();
+            if (offset < 0) throw new RuntimeException(node + " analysis failed, Maybe it is larger than Long.MAX_VALUE");
+        }
+        json.addProperty("offset", offset);
+    }
+
+    private static void analysisLimit(SqlNode node, JsonObject json) {
+        long limit = -1L;
+        if (node instanceof SqlNumericLiteral) {
+            limit = ((SqlNumericLiteral) node).bigDecimalValue().longValue();
+            if (limit < 0) throw new RuntimeException(node + " analysis failed, Maybe it is larger than Long.MAX_VALUE");
+//            limit = ((SqlNumericLiteral) node).intValue(false);
+        }
+        json.addProperty("limit", limit);
+    }
+
+
+
 }
+
+
+
---
+++ core/src/test/java/com/hzcominfo/dataggr/uniquery/test/SqlExplainerTest.java
@@ -0,0 +1,194 @@
+package com.hzcominfo.dataggr.uniquery.test;
+
+import com.google.gson.JsonObject;
+import com.hzcominfo.dataggr.uniquery.SqlExplainer;
+import org.apache.calcite.jdbc.CalcitePrepare;
+import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.parser.SqlParseException;
+import org.apache.calcite.sql.parser.SqlParser;
+import org.apache.calcite.sql.util.SqlBasicVisitor;
+import org.junit.Test;
+
+public class SqlExplainerTest {
+
+    private SqlParser.ConfigBuilder parserConfig = SqlExplainer.DEFAULT_PARSER_CONFIG;
+    private SqlBasicVisitor<String> visitor = SqlExplainer.visitor;
+
+    @Test
+    public void t1() throws SqlParseException {
+//        String sql = "select p.name AS n, p.age As a, p.address, count(name) from people p where age > 30  and name like '%姓名%' order by age, name desc limit 10, 999";
+        String sql = "select p.name AS n, p.age As a, p.address from people p where (age > 30  or name like '%姓名%') and name like '%xxx%' order by age, name desc limit 1000 offset 100";
+//        String sql = "select p.name AS n, p.age As a, p.address, count(name) from people p where age > ?  and name like '%姓名%' order by age, name desc offset ? row fetch first ? rows only";
+//        String sql = "select distinct(name) from people where age > 30";
+
+        SqlParser parser = SqlParser.create(sql, parserConfig.build());
+        SqlNode query = parser.parseQuery();
+
+        System.out.println("---------" + query.getKind());
+        query.accept(visitor);
+//        SqlNode stmt = parser.parseStmt();
+//        stmt.accept(visitor);
+
+    }
+
+    @Test
+    public void t2() {
+        String sql = "select p.name AS n, p.age As a, p.address from people p where age > 30  and name like '%姓名%' order by age desc, name asc limit 1000 offset 10";
+        CalcitePrepare.Query query = CalcitePrepare.Query.of(sql);
+    }
+
+    @Test
+    public void t3() throws Exception {
+        String sql = "INSERT INTO Persons VALUES ('Gates', 'Bill', 'Xuanwumen 10', 'Beijing')";
+//        SqlNode query = SqlExplainer.parseSql(sql);
+//        query.accept(SqlExplainer.visitor);
+    }
+
+    @Test
+    public void t4() throws Exception {
+        String sql = "UPDATE Person SET FirstName = 'Fred' WHERE LastName = 'Wilson'";
+//        SqlNode update = SqlExplainer.parseSql(sql);
+//        update.accept(SqlExplainer.visitor);
+    }
+
+    @Test
+    public void t5() throws Exception {
+        String sql = "select p.name AS n, p.age As a, p.address from people p where age > 30  and name like '%姓名%' order by age, name desc limit 1000 offset 100";
+//        SqlNode query = SqlExplainer.parseSql(sql, "Lucy", 27, "W");
+//        System.out.println(query);
+//        System.out.println();
+//        String s = query.accept(SqlExplainer.visitor);
+//        System.out.println("s===" + s);
+        JsonObject object = SqlExplainer.parseSql(sql);
+    }
+
+    @Test
+    public void p1() throws Exception {
+//        String sql = "select * from tbl where name like '%tdl%'";
+//        String sql = "select name, age, sex as s from tbl where name like '%tdl%'";
+        String sql = "select name, age, *, sex as s from tbl where name like '%tdl%'";
+//        String sql = "select * from tbl AS t, wsy as w, abc where name like '%tdl%'";
+//        String sql = "select * from tbl1 as x, tbl2 as y, tbl3 where name like '%tdl%'";
+        JsonObject object = SqlExplainer.parseSql(sql);
+    }
+
+    @Test
+    public void p2() throws Exception {
+        String sql = "SELECT Customer, OrderDate, SUM(OrderPrice) FROM Orders GROUP BY Customer,OrderDate";
+        JsonObject object = SqlExplainer.parseSql(sql);
+        System.out.println(object);
+    }
+
+    @Test
+    public void p3() throws Exception {
+        String sql = "SELECT Customer,SUM(OrderPrice) FROM Orders GROUP BY Customer HAVING SUM(OrderPrice)<2000 limit 1000 offset 100";
+        JsonObject object = SqlExplainer.parseSql(sql);
+        System.out.println(object);
+    }
+
+    @Test
+    public void p4() throws Exception {
+        String sql = "SELECT Company, OrderNumber FROM Orders,tbl2,tbl3 ORDER BY Company desc, xxx desc limit 1000 offset 100";
+        JsonObject object = SqlExplainer.parseSql(sql);
+        System.out.println(object);
+    }
+
+    @Test
+    public void p5() throws Exception {
+        String sql = "SELECT DISTINCT Company, name FROM Orders order by xxx limit 1000 offset 100";
+        JsonObject object = SqlExplainer.parseSql(sql);
+        System.out.println(object);
+    }
+
+    @Test
+    public void p6() throws Exception {
+        String sql = "SELECT name as n, Company as c, pure FROM Orders order by xxx limit 1000 offset 100";
+        JsonObject object = SqlExplainer.parseSql(sql);
+        System.out.println(object);
+    }
+
+    /**
+     * where
+     * @throws Exception
+     */
+    @Test
+    public void w1() throws Exception {
+//        String sql = "select * from t where name = 'ci' and age = 30";
+//        String sql = "select * from t where (name = 'ci' or age < 30) and (name = 'xx' or age >= 20) and (name like '%yy%' or age <> 10)";
+//        String sql = "select * from t where name is not NULL";
+//        String sql = "select * from t where name is null";
+//        String sql = "select * from t where age >= 10";
+//        String sql = "select * from t where name in (a, b, c, d, e)"; //todo wrong syntax
+        String sql = "select * from t where name not in ('a', 'b', 'c', 'd', 'e') and (name is NULL or age < 20) or (name = 'xx' or age >= 20) and (name like '%yy%' or age <> 10)";
+//        String sql = "select * from t where name in ('a', 'b', 'c', 'd', 'e')";
+        JsonObject object = SqlExplainer.parseSql(sql);
+        JsonObject o = object.getAsJsonObject("where").getAsJsonArray("and").get(0).getAsJsonObject()
+                .getAsJsonArray("and").get(0).getAsJsonObject().getAsJsonArray("or")
+                .get(0).getAsJsonObject().getAsJsonObject("equals");
+        o.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue().getAsString());
+        });
+    }
+
+    /**
+     * group by
+     * @throws Exception
+     */
+    @Test
+    public void g1() throws Exception {
+        String sql = "SELECT Customer,OrderPrice FROM Orders GROUP BY Customer, xxxx";
+        JsonObject object = SqlExplainer.parseSql(sql);
+    }
+
+    /**
+     * having
+     * @throws Exception
+     */
+    @Test
+    public void h1() throws Exception {
+        String sql = "SELECT Customer,OrderPrice FROM Orders GROUP BY Customer HAVING SUM(OrderPrice)<2000";
+        JsonObject object = SqlExplainer.parseSql(sql);
+    }
+
+    /**
+     * order by
+     * @throws Exception  order by
+     */
+    @Test
+    public void ob1() throws Exception {
+//        String sql = "SELECT Company, OrderNumber FROM Orders ORDER BY Company ";
+//        String sql = "SELECT Company, OrderNumber FROM Orders ORDER BY Company DESC";
+        String sql = "SELECT Company, OrderNumber FROM Orders ORDER BY Company DESC, ABC DESC";
+//        String sql = "SELECT Company, OrderNumber FROM Orders ORDER BY Company DESC, OrderNumber ASC";
+//        String sql = "SELECT Company, OrderNumber FROM Orders ORDER BY Company Asc, OrderNumber desc";
+        JsonObject object = SqlExplainer.parseSql(sql);
+    }
+
+    /**
+     * offset
+     * @throws Exception
+     */
+    @Test
+    public void of1() throws Exception {
+//        String sql = "SELECT Customer,OrderPrice FROM Orders";
+        String sql = "SELECT Customer,OrderPrice FROM Orders limit 99887766";
+//        String sql = "SELECT Customer,OrderPrice FROM Orders limit 92233720368547758070 offset 9223372036854775807";
+//        String sql = "SELECT Customer,OrderPrice FROM Orders limit 9223372036854775807 offset 1024";
+        JsonObject object = SqlExplainer.parseSql(sql);
+    }
+
+    /**
+     * limit
+     * @throws Exception
+     */
+    @Test
+    public void l1() throws Exception {
+//        String sql = "SELECT Customer,OrderPrice FROM Orders limit 92233720368547758070 offset 9223372036854775807";
+        String sql = "SELECT Customer,OrderPrice FROM Orders limit 9223372036854775807 offset 1024";
+        JsonObject object = SqlExplainer.parseSql(sql);
+    }
+}
+
+
+// https://github.com/publicclass/sql-where
