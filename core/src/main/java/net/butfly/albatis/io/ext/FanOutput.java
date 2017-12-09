2d6752f060856472bf159de3292c343f69c7e1d5
कलविङ्क <zhangx@hzcominfo.com>
2017-12-09 16:24:10 +0800

    make key output work parals fine

--- core/src/main/java/net/butfly/albatis/io/ext/FanOutput.java
+++ core/src/main/java/net/butfly/albatis/io/ext/FanOutput.java
@@ -2,12 +2,17 @@ package net.butfly.albatis.io.ext;
 
 import static net.butfly.albacore.paral.Sdream.of;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 import net.butfly.albacore.base.Namedly;
+import net.butfly.albacore.paral.Exeter;
 import net.butfly.albacore.paral.Sdream;
+import net.butfly.albacore.utils.collection.Colls;
 import net.butfly.albatis.io.Output;
 
 public class FanOutput<V> extends Namedly implements Output<V> {
-	private final Iterable<? extends Output<V>> outputs;
+	private final List<Consumer<Sdream<V>>> tasks;
 
 	public FanOutput(Iterable<? extends Output<V>> outputs) {
 		this("FanOutTo" + ":" + of(outputs).joinAsString(Output::name, "&"), outputs);
@@ -16,12 +21,13 @@ public class FanOutput<V> extends Namedly implements Output<V> {
 
 	public FanOutput(String name, Iterable<? extends Output<V>> outputs) {
 		super(name);
-		this.outputs = outputs;
+		tasks = Colls.list();
+		for (Output<V> o : outputs)
+			tasks.add(s -> o.enqueue(s));
 	}
 
 	@Override
 	public void enqueue(Sdream<V> s) {
-		for (Output<V> o : outputs)
-			o.enqueue(s);
+		Exeter.of().join(s, tasks);
 	}
 }
