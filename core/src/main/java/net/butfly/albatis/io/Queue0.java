2b487667b8e364b914e4000caa53a8047b3df657
कलविङ्क <zhangx@hzcominfo.com>
2018-01-12 15:36:48 +0800

    format code by sort methods -_-

--- core/src/main/java/net/butfly/albatis/io/Queue0.java
+++ core/src/main/java/net/butfly/albatis/io/Queue0.java
@@ -73,6 +73,12 @@ public interface Queue0<I, O> extends Input<O>, Output<I> {
 		return i;
 	}
 
+	@Override
+	default <O1> Queue0<I, O1> thens(Function<Sdream<O>, Sdream<O1>> conv) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	@Deprecated
 	@Override
 	default <O1> Queue0<I, O1> thens(Function<Sdream<O>, Sdream<O1>> conv, int parallelism) {
@@ -144,6 +150,12 @@ public interface Queue0<I, O> extends Input<O>, Output<I> {
 		return o;
 	}
 
+	@Override
+	default <I0> Queue0<I0, O> priors(Function<Sdream<I0>, Sdream<I>> conv) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	@Deprecated
 	@Override
 	default <I0> Queue0<I0, O> priors(Function<Sdream<I0>, Sdream<I>> conv, int parallelism) {
@@ -178,19 +190,7 @@ public interface Queue0<I, O> extends Input<O>, Output<I> {
 	}
 
 	@Override
-	default <I0> Queue0<I0, O> priors(Function<Sdream<I0>, Sdream<I>> conv) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	default <O1> Queue0<I, O1> thens(Function<Sdream<O>, Sdream<O1>> conv) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	default <O1> Queue0<I, O1> thenFlat(Function<O, Sdream<O1>> conv) {
+	default <I0> Queue0<I0, O> priorFlat(Function<I0, Sdream<I>> conv) {
 		// TODO Auto-generated method stub
 		return null;
 	}
