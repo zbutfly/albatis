b4dd3943e37687287fa85e172ddab83dd5728e21
कलविङ्क <zhangx@hzcominfo.com>
2018-02-04 23:39:12 +0800

    add KafkaConnection for unify db i/o

---
+++ kafka-io/src/main/java/net/butfly/albatis/kafka/KafkaConnection.java
@@ -0,0 +1,54 @@
+package net.butfly.albatis.kafka;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.hzcominfo.albatis.nosql.NoSqlConnection;
+
+import net.butfly.albacore.exception.ConfigException;
+import net.butfly.albacore.io.URISpec;
+import net.butfly.albacore.serder.BsonSerder;
+import net.butfly.albacore.utils.collection.Colls;
+import net.butfly.albatis.io.Input;
+import net.butfly.albatis.io.Message;
+import net.butfly.albatis.io.Output;
+
+public class KafkaConnection extends NoSqlConnection<Object> {
+	public KafkaConnection(URISpec uri) throws IOException {
+		super(uri, u -> null, "kafka");
+	}
+
+	@Override
+	public Input<Message> input(String... topic) throws IOException {
+		try {
+			return new KafkaInput("KafkaInput", uri, BsonSerder::map, topic);
+		} catch (ConfigException e) {
+			throw new IOException(e);
+		}
+	}
+
+	@Override
+	public Output<Message> output() throws IOException {
+		try {
+			return new KafkaOutput("KafkaInput", uri, BsonSerder::map);
+		} catch (ConfigException e) {
+			throw new IOException(e);
+		}
+	}
+
+	public static class Driver implements com.hzcominfo.albatis.nosql.Connection.Driver<KafkaConnection> {
+		static {
+			DriverManager.register(new Driver());
+		}
+
+		@Override
+		public KafkaConnection connect(URISpec uriSpec) throws IOException {
+			return new KafkaConnection(uriSpec);
+		}
+
+		@Override
+		public List<String> schemas() {
+			return Colls.list("mongodb");
+		}
+	}
+}
