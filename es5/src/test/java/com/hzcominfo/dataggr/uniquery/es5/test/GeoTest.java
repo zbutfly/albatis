afa4d5e1e7ea87dd431c6250935ea4539cab46e4
chenw <chenw@hzcominfo.com>
2018-03-13 11:53:53 +0800

    geo_distance test

---
+++ es5/src/test/java/com/hzcominfo/dataggr/uniquery/es5/test/GeoTest.java
@@ -0,0 +1,15 @@
+package com.hzcominfo.dataggr.uniquery.es5.test;
+
+import org.elasticsearch.common.geo.GeoDistance;
+import org.elasticsearch.common.unit.DistanceUnit;
+import org.elasticsearch.index.query.QueryBuilders;
+
+public class GeoTest {
+
+	public static void main(String[] args) {
+		// geo_distance(field,lat,lon,d);
+		QueryBuilders.geoDistanceQuery("field")  
+	    .point(Double.valueOf("lat"), Double.valueOf("lon"))                                 
+	    .distance(Double.valueOf("distance"), DistanceUnit.KILOMETERS).geoDistance(GeoDistance.PLANE);
+	}
+}
