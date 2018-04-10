78150d4f2dcfd3c96f6972c784d0971d483644ce
कलविङ्क <zhangx@hzcominfo.com>
2018-04-10 16:29:19 +0800

    use completable future for arango vertex before edge

--- arangodb-io/src/main/java/net/butfly/albatis/arangodb/ArangoOutput.java
+++ arangodb-io/src/main/java/net/butfly/albatis/arangodb/ArangoOutput.java
@@ -1,22 +1,18 @@
 package net.butfly.albatis.arangodb;
 
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicReference;
 
 import com.arangodb.ArangoCursorAsync;
 import com.arangodb.entity.BaseDocument;
 
-import net.butfly.albacore.paral.Exeter;
 import net.butfly.albacore.paral.Sdream;
-import net.butfly.albacore.utils.collection.Colls;
 import net.butfly.albacore.utils.collection.Maps;
 import net.butfly.albatis.io.Message;
 import net.butfly.albatis.io.OutputBase;
 
-public class ArangoOutput extends OutputBase<Message> {
+public class ArangoOutput extends OutputBase<EdgeMessage> {
 	private final ArangoConnection conn;
 
 	protected ArangoOutput(String name, ArangoConnection conn) {
@@ -25,39 +21,27 @@ public class ArangoOutput extends OutputBase<Message> {
 	}
 
 	@Override
-	protected void enqueue0(Sdream<Message> items) {
-		Map<String, List<Message>> m = Maps.of();
-		List<Future<?>> fs = Colls.list();
-		items.eachs(e -> {
-			if (e instanceof EdgeMessage) //
-				fs.add(writeEdgeWithVertexes((EdgeMessage) e));
-			else m.compute(e.table(), (t, o) -> {
-				if (null == o) o = Colls.list();
-				o.add(e);
-				return o;
+	protected void enqueue0(Sdream<EdgeMessage> edges) {
+		List<CompletableFuture<ArangoCursorAsync<BaseDocument>>> l = edges.map(e -> {
+			Message v;
+			CompletableFuture<ArangoCursorAsync<BaseDocument>> f = null;
+			if (null != (v = e.start())) f = async(v);
+			if (null != (v = e.end())) f = null == f ? async(v) : f.thenCombine(async(v), (c1, c2) -> null);
+			return f.thenCompose(n -> {
+				AtomicReference<CompletableFuture<ArangoCursorAsync<BaseDocument>>> ref = new AtomicReference<>(async(e));
+				e.edges.forEach(ee -> ref.set(ref.get().thenCombine(async(ee), (c1, c2) -> null)));
+				return ref.get();
 			});
-		});
-		Sdream.of(m).eachs(e -> fs.add(conn.db.collection(e.getKey()).insertDocuments(e.getValue())));
-		for (Future<?> f : fs)
-			try {
-				f.get();
-			} catch (InterruptedException e) {} catch (ExecutionException e) {
-				e.getCause().printStackTrace();;
-			}
+		}).list();
+		CompletableFuture.allOf(l.toArray(new CompletableFuture[l.size()]));
 	}
 
 	private static final String EDGE_AQL = "upsert data._key == @key insert @data update {} in @col";
 
-	private Future<?> writeEdgeWithVertexes(EdgeMessage edge) {
-		return Exeter.of().submit(() -> {
-			CompletableFuture<ArangoCursorAsync<BaseDocument>> f0 = conn.client().db().query(EDGE_AQL, Maps.of("data", edge.vertexes[0]
-					.map(), "col", edge.vertexes[0].table(), "key", edge.vertexes[0].key()), null, BaseDocument.class);
-			CompletableFuture<ArangoCursorAsync<BaseDocument>> f1 = conn.client().db().query(EDGE_AQL, Maps.of("data", edge.vertexes[1]
-					.map(), "col", edge.vertexes[1].table(), "key", edge.vertexes[1].key()), null, BaseDocument.class);
-			f0.join();
-			f1.join();
-			conn.client().db().query(EDGE_AQL, Maps.of("data", edge.map(), "col", edge.table(), "key", edge.key()), null,
-					BaseDocument.class).join();
-		});
+	private CompletableFuture<ArangoCursorAsync<BaseDocument>> async(Message v) {
+		CompletableFuture<ArangoCursorAsync<BaseDocument>> f = conn.client().db().query(EDGE_AQL, Maps.of("data", v.map(), "col", v.table(),
+				"key", v.key()), null, BaseDocument.class);
+		// ArangoCursorAsync<BaseDocument> cc = f.get();
+		return f;
 	}
 }
--- arangodb-io/src/main/java/net/butfly/albatis/arangodb/EdgeMessage.java
+++ arangodb-io/src/main/java/net/butfly/albatis/arangodb/EdgeMessage.java
@@ -1,16 +1,20 @@
 package net.butfly.albatis.arangodb;
 
+import java.util.List;
 import java.util.Map;
 
+import net.butfly.albacore.utils.collection.Colls;
 import net.butfly.albatis.io.Message;
 
 public class EdgeMessage extends Message {
 	private static final long serialVersionUID = -7016058550224590870L;
-	public final Message[] vertexes;
+	final Message[] vertexes;
+	final List<Message> edges;
 
 	public EdgeMessage(String tbl, Object key, Map<String, Object> values) {
 		super(tbl, key, values);
 		vertexes = new Message[] { null, null };
+		edges = Colls.list();
 	}
 
 	public void start(String tbl, Object key, Map<String, Object> values) {
@@ -19,5 +23,17 @@ public class EdgeMessage extends Message {
 
 	public void end(String tbl, Object key, Map<String, Object> values) {
 		vertexes[1] = new Message(tbl, key, values);
+	}
+
+	public Message start() {
+		return vertexes[0];
+	}
+
+	public Message end() {
+		return vertexes[1];
+	}
+
+	public void edge(Message edge) {
+		edges.add(edge);
 	}
 }
