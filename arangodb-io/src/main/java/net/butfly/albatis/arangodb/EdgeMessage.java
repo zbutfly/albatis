65d909c0f31491e74520c82ab90739a4330abd36
कलविङ्क <zhangx@hzcominfo.com>
2018-04-15 16:23:54 +0800

    optimize code for arango nested generating

--- arangodb-io/src/main/java/net/butfly/albatis/arangodb/EdgeMessage.java
+++ arangodb-io/src/main/java/net/butfly/albatis/arangodb/EdgeMessage.java
@@ -1,57 +1,81 @@
 package net.butfly.albatis.arangodb;
 
-import java.util.Collection;
+import java.text.Format;
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import com.arangodb.entity.BaseDocument;
 
 import net.butfly.albacore.utils.collection.Colls;
+import net.butfly.albacore.utils.collection.Maps;
 import net.butfly.albatis.io.Message;
 
 public class EdgeMessage extends Message {
 	private static final long serialVersionUID = -7016058550224590870L;
-	final Message[] vertexes;
-	final List<Message> edges;
-	List<NestedAqls> nested = Colls.list();
 
-	public EdgeMessage(String tbl, Object key, Map<String, Object> values) {
-		super(tbl, key, values);
-		vertexes = new Message[] { null, null };
-		edges = Colls.list();
+	private static final Format AQL_UPSERT = new MessageFormat("upsert '{'_key: @_key} insert {1} update {1} in {0} return NEW"); //
+	private EdgeMessage start, end;
+	final String aql;
+	private Function<Map<String, Object>, List<EdgeMessage>> nestedThen;
+
+	public EdgeMessage(String tbl, Object key, Map<String, Object> vertex) {
+		super(tbl, key, vertex);
+		vertex.put("_key", key);
+		aql = AQL_UPSERT.format(new String[] { tbl, parseAqlAsBindParams(vertex) });
+	}
+
+	public EdgeMessage(String aql, Map<String, Object> params) {
+		super(params);
+		this.aql = aql;
+	}
+
+	public EdgeMessage(String aql) {
+		super();
+		this.aql = aql;
 	}
 
 	public void start(String tbl, Object key, Map<String, Object> values) {
-		vertexes[0] = new Message(tbl, key, values);
+		start = new EdgeMessage(tbl, key, values);
 	}
 
 	public void end(String tbl, Object key, Map<String, Object> values) {
-		vertexes[1] = new Message(tbl, key, values);
+		end = new EdgeMessage(tbl, key, values);
 	}
 
-	public Message start() {
-		return vertexes[0];
+	public EdgeMessage start() {
+		return start;
 	}
 
-	public Message end() {
-		return vertexes[1];
+	public EdgeMessage end() {
+		return end;
 	}
 
-	public void edge(Message edge) {
-		edges.add(edge);
+	public boolean nested() {
+		return null != nestedThen;
+	}
+
+	public EdgeMessage then(Function<Map<String, Object>, List<EdgeMessage>> nestedThen) {
+		this.nestedThen = nestedThen;
+		return this;
+	}
+
+	public List<EdgeMessage> applyThen(BaseDocument doc) {
+		return null == nestedThen ? Colls.list() : nestedThen.apply(null == doc ? Maps.of() : doc.getProperties());
 	}
 
 	@Override
 	public String toString() {
 		return super.toString() //
-				+ (null == vertexes[0] ? "" : "\n\tStart Vertex: " + vertexes[0].toString()) //
-				+ (null == vertexes[1] ? "" : "\n\t  End Vertex: " + vertexes[1].toString()) //
-				+ (edges.isEmpty() ? "" : "\n\t\t And [" + edges.size() + "] other edges.");
+				+ (null == start ? "" : "\n\tStart Vertex: " + start.toString()) //
+				+ (null == end ? "" : "\n\t  End Vertex: " + end.toString());
 	}
 
-	public void edges(Collection<? extends Message> edges) {
-		this.edges.addAll(edges);
-	}
+	private static String parseAqlAsBindParams(Map<String, Object> v) {
+		return "{" + v.keySet().stream().map(
 
-	public void then(List<NestedAqls> andThens) {
-		this.nested.addAll(andThens);
+				k -> k + ": @" + k).collect(Collectors.joining(", ")) + "}";
 	}
 }
