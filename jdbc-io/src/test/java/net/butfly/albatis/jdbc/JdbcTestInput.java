07632d45a02579bcff3d70f5f9d1c54e3d98b654
taideli <taidl@hzcominfo.com>
2018-04-16 17:14:47 +0800

    jdbc output

--- jdbc-io/src/test/java/net/butfly/albatis/jdbc/JdbcTestInput.java
+++ jdbc-io/src/test/java/net/butfly/albatis/jdbc/JdbcTestInput.java
@@ -4,6 +4,7 @@ import net.butfly.albacore.base.Namedly;
 import net.butfly.albatis.io.Message;
 import net.butfly.albatis.io.OddInput;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -12,18 +13,15 @@ import java.util.stream.Stream;
 public class JdbcTestInput extends Namedly implements OddInput<Message> {
     private List<Message> list;
 
-    /**
-     * for mysql, all field should be upper case
-     * @param name
-     */
     public JdbcTestInput(String name) {
         super(name);
         list = Stream.iterate(1, i -> i + 1).limit(20).map(i -> {
-            Message m = new Message("ATEST");
-            m.put("ID", i);
+            Message m = new Message("test");
+            m.put("id", i);
             m.key(i);
-            m.put("NAME", UUID.randomUUID().toString());
-            if (i % 2 == 0) m.put("ADDRESS", UUID.randomUUID().toString());
+            m.put("name", UUID.randomUUID().toString());
+            if (i % 2 == 0) m.put("address", UUID.randomUUID().toString());
+            if (i % 5 == 0) m.put("id", "" + i);
             return m;
         }).collect(Collectors.toList());
         open();
