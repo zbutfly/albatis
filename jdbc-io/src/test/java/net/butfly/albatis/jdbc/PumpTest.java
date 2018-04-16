be7ae6a495f355257079a95e3784b486e9b5868d
taideli <taidl@hzcominfo.com>
2018-04-16 20:49:18 +0800

    use HikariCP as JdbcOutput connection pool

--- jdbc-io/src/test/java/net/butfly/albatis/jdbc/PumpTest.java
+++ jdbc-io/src/test/java/net/butfly/albatis/jdbc/PumpTest.java
@@ -2,19 +2,18 @@ package net.butfly.albatis.jdbc;
 
 import net.butfly.albatis.io.pump.Pump;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.net.URISyntaxException;
 import java.sql.SQLException;
 
 public class PumpTest {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, URISyntaxException {
         JdbcTestInput input = new JdbcTestInput("input");
-        Connection conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/postgres",
-                "postgres", "!@#QAZ123qaz");
-
-        JdbcOutput output = new JdbcOutput("output", conn, false);
+        String uri = "jdbc:mysql://ph_warning_rhfl_test:Ph_warning_rhfl@test123!@172.16.17.14:3306/ph_warning_rhfl_test?useSSL=false";
+        JdbcOutput output = new JdbcOutput("output", uri, false);
         Pump pump = Pump.pump(input, 3, output);
         pump.open();
+
+        System.out.println("wait...");
     }
 }
