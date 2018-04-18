baa1a6aff08e1cba7505759339d1e6de4b6eae22
taideli <taidl@hzcominfo.com>
2018-04-18 20:35:57 +0800

    file rename

--- jdbc-io/src/test/java/net/butfly/albatis/jdbc/PumpTest4Mysql.java
+++ jdbc-io/src/test/java/net/butfly/albatis/jdbc/PumpTest4Mysql.java
@@ -5,7 +5,7 @@ import net.butfly.albatis.io.pump.Pump;
 import java.net.URISyntaxException;
 import java.sql.SQLException;
 
-public class PumpTest {
+public class PumpTest4Mysql {
 
     public static void main(String[] args) throws SQLException, URISyntaxException {
         JdbcTestInput input = new JdbcTestInput("input");
