ca7e0eba77618d477a30f9779729d29a42806d2c
taideli <taidl@hzcominfo.com>
2018-04-20 11:30:44 +0800

    postgres upsert

---
+++ jdbc-io/src/test/java/net/butfly/albatis/jdbc/PumpTest4Postgres.java
@@ -0,0 +1,19 @@
+package net.butfly.albatis.jdbc;
+
+import net.butfly.albatis.io.pump.Pump;
+
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+
+public class PumpTest4Postgres {
+
+    public static void main(String[] args) throws SQLException, URISyntaxException {
+        JdbcTestInput input = new JdbcTestInput("input");
+        String uri = "jdbc:postgresql://test:Test001!@127.0.0.1:5432/postgres";
+        JdbcOutput2 output = new JdbcOutput2("output", uri);
+        Pump pump = Pump.pump(input, 3, output);
+        pump.open();
+
+        System.out.println("wait...");
+    }
+}
