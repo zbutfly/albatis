9277d4a591d7b6e95d4e2e0217ca187ec2d025f9
कलविङ्क <zhangx@hzcominfo.com>
2018-05-01 17:59:36 +0800

    change all io (basic) proj to 3.0.3 release

--- neo4j-io/src/main/java/net/butfly/albatis/neo4j/Neo4jConnection.java
+++ neo4j-io/src/main/java/net/butfly/albatis/neo4j/Neo4jConnection.java
@@ -1,27 +1,52 @@
 package net.butfly.albatis.neo4j;
 
 import java.io.IOException;
-import java.util.function.Function;
+import java.util.List;
 
-import org.neo4j.driver.v1.AuthToken;
 import org.neo4j.driver.v1.AuthTokens;
-import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
 
 import com.hzcominfo.albatis.nosql.NoSqlConnection;
 
 import net.butfly.albacore.io.URISpec;
+import net.butfly.albacore.utils.collection.Colls;
+import net.butfly.albatis.io.Input;
+import net.butfly.albatis.io.Message;
+import net.butfly.albatis.io.Output;
 
-public class Neo4jConnection extends NoSqlConnection<Driver> {
-	public Neo4jConnection(URISpec uri, Function<URISpec, Driver> client) throws IOException {
-		super(uri, u -> {
-			AuthToken auth = AuthTokens.basic(u.getUsername(), u.getPassword());
-			return GraphDatabase.driver(u.toString(), auth);
-		}, 7687, "bolt", "neo4j");
+public class Neo4jConnection extends NoSqlConnection<org.neo4j.driver.v1.Driver> {
+	public Neo4jConnection(URISpec uri) throws IOException {
+		super(uri, u -> GraphDatabase.driver(u.toString(), AuthTokens.basic(u.getUsername(), u.getPassword())), 7687, "bolt", "neo4j");
 	}
 
 	@Override
 	public void close() throws IOException {
 		client().close();
+	}
+
+	@Override
+	public <M extends Message> Input<M> input(String... table) throws IOException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public <M extends Message> Output<M> output() throws IOException {
+		throw new UnsupportedOperationException();
+	}
+
+	public static class Driver implements com.hzcominfo.albatis.nosql.Connection.Driver<Neo4jConnection> {
+		static {
+			DriverManager.register(new Driver());
+		}
+
+		@Override
+		public Neo4jConnection connect(URISpec uriSpec) throws IOException {
+			return new Neo4jConnection(uriSpec);
+		}
+
+		@Override
+		public List<String> schemas() {
+			return Colls.list("neo4j", "bolt");
+		}
 	}
 }
