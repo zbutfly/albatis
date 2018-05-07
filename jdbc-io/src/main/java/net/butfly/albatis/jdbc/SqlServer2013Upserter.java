2ead1e62c821f6f716653685ce8f46e4615e4fba
taideli <taidl@hzcominfo.com>
2018-05-07 16:19:40 +0800

    SQLServer support

--- jdbc-io/src/main/java/net/butfly/albatis/jdbc/SqlServer2013Upserter.java
+++ jdbc-io/src/main/java/net/butfly/albatis/jdbc/SqlServer2013Upserter.java
@@ -1,30 +1,7 @@
 package net.butfly.albatis.jdbc;
 
-import net.butfly.albacore.io.URISpec;
-import net.butfly.albatis.io.Message;
-
-import java.sql.Connection;
-import java.util.List;
-import java.util.Map;
-
-public class SqlServer2013Upserter extends Upserter {
+public class SqlServer2013Upserter extends SqlServer2005Upserter {
     public SqlServer2013Upserter(Type type) {
         super(type);
     }
-
-    @Override
-    String urlAssemble(URISpec uriSpec) {
-        return null;
-    }
-
-    @Override
-    String urlAssemble(String schema, String host, String database) {
-        return null;
-    }
-
-    @Override
-    long upsert(Map<String, List<Message>> mml, Connection conn) {
-        return 0;
-    }
-
 }
