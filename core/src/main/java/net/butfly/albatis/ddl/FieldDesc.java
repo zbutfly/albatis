a6cdf4819086ff41d0c6f6f293e2b0debd7c7d23
कलविङ्क <zhangx@hzcominfo.com>
2018-05-27 23:42:21 +0800

    for test, and add kudu table construct

--- core/src/main/java/net/butfly/albatis/ddl/FieldDesc.java
+++ core/src/main/java/net/butfly/albatis/ddl/FieldDesc.java
@@ -22,7 +22,7 @@ public final class FieldDesc {
 	// extra
 	private final Map<String, Object> attrs = Maps.of();
 
-	public FieldDesc(String fullname, ValType type, boolean rowkey, boolean unique, boolean nullable) {
+	public FieldDesc(String name, ValType type, boolean rowkey, boolean unique, boolean nullable) {
 		super();
 		String[] cfPrefixName = parse(fullname);
 		this.name = cfPrefixName[2];
@@ -31,11 +31,10 @@ public final class FieldDesc {
 		this.rowkey = rowkey;
 		this.unique = unique;
 		this.nullable = rowkey ? false : nullable;
-		attr("cf", cfPrefixName[0]).attr("prefix", cfPrefixName[1]);
 	}
 
-	public FieldDesc(String fullname, ValType type, boolean rowkey) {
-		this(fullname, type, rowkey, false, !rowkey);
+	public FieldDesc(String name, ValType type, boolean rowkey) {
+		this(name, type, rowkey, false, !rowkey);
 	}
 
 	public FieldDesc(String fullname, ValType type) {
