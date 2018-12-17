7b700bc55f81a8230ad6aa94002dcbd552206fe9
lilz <lilz@hzcominfo.com>
2018-12-17 18:38:56 +0800

    add URISpec parameter inputSeriar=string for KafkaStrInput

--- kafka-io/src/main/java/net/butfly/albatis/kafka/KafkaConnection.java
+++ kafka-io/src/main/java/net/butfly/albatis/kafka/KafkaConnection.java
@@ -1,21 +1,21 @@
 package net.butfly.albatis.kafka;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.kafka.common.security.JaasUtils;
+
 import com.hzcominfo.albatis.nosql.Connection;
 import com.hzcominfo.albatis.nosql.NoSqlConnection;
-import kafka.admin.AdminUtils;
-import kafka.admin.RackAwareMode;
+
 import kafka.utils.ZkUtils;
 import net.butfly.albacore.exception.ConfigException;
 import net.butfly.albacore.io.URISpec;
 import net.butfly.albacore.utils.collection.Colls;
-import net.butfly.albatis.ddl.FieldDesc;
 import net.butfly.albatis.ddl.TableDesc;
-import org.apache.kafka.common.security.JaasUtils;
+import net.butfly.albatis.io.Input;
+import net.butfly.albatis.io.Rmap;
 import scala.collection.JavaConversions;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Properties;
 
 public class KafkaConnection extends NoSqlConnection<Connection> {
     public KafkaConnection(URISpec uri) throws IOException {
@@ -23,9 +23,11 @@ public class KafkaConnection extends NoSqlConnection<Connection> {
     }
 
     @Override
-    public KafkaInput input(TableDesc... topic) throws IOException {
+    public Input<Rmap> input(TableDesc... topic) throws IOException {
         List<String> l = Colls.list(t -> t.name, topic);
         try {
+        	if (null != uri.getParameter("inputSeriar") && "string".equals(uri.getParameter("inputSeriar")))
+        		return new KafkaStrInput("KafkaStrInput", uri, l.toArray(new String[l.size()]));
             return new KafkaInput("KafkaInput", uri, l.toArray(new String[l.size()]));
         } catch (ConfigException e) {
             throw new IOException(e);
@@ -62,16 +64,16 @@ public class KafkaConnection extends NoSqlConnection<Connection> {
         return null;
     }
 
-    @Override
-    public void construct(String dbName, String table, TableDesc tableDesc, List<FieldDesc> fields) {
-        String kafkaUrl = uri.getHost()+"/kafka";
-        ZkUtils zkUtils = ZkUtils.apply(kafkaUrl, 30000, 30000, JaasUtils.isZkSecurityEnabled());
-        Integer partition = (Integer) tableDesc.construct.get("partition");
-        Integer replication = (Integer) tableDesc.construct.get("replication");
-        AdminUtils.createTopic(zkUtils, table, partition, replication, new Properties(), new RackAwareMode.Enforced$());
-        logger().info("create kafka topic successful");
-        zkUtils.close();
-    }
+//    @Override
+//    public void construct(String dbName, String table, TableDesc tableDesc, List<FieldDesc> fields) {
+//        String kafkaUrl = uri.getHost()+"/kafka";
+//        ZkUtils zkUtils = ZkUtils.apply(kafkaUrl, 30000, 30000, JaasUtils.isZkSecurityEnabled());
+//        Integer partition = (Integer) tableDesc.construct.get("partition");
+//        Integer replication = (Integer) tableDesc.construct.get("replication");
+//        AdminUtils.createTopic(zkUtils, table, partition, replication, new Properties(), new RackAwareMode.Enforced$());
+//        logger().info("create kafka topic successful");
+//        zkUtils.close();
+//    }
 
     @Override
     public boolean judge(String dbName, String table) {
