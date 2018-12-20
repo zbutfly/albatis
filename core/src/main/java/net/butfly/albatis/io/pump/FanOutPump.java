405436885cf54522d898b69b52eb6d578ab278c7
कलविङ्क <zhangx@hzcominfo.com>
2018-12-20 15:49:46 +0800

    clean and split

--- core/src/main/java/net/butfly/albatis/io/pump/FanOutPump.java
+++
@@ -1,8 +0,0 @@
-package net.butfly.albatis.io.pump;
-
-//TODO
-public class FanOutPump<V> extends PumpImpl<V, FanOutPump<V>> {
-	protected FanOutPump(String name, int parallelism) {
-		super(name, parallelism);
-	}
-}
