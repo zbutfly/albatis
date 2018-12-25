16c7452019478e72c1528bcfb5f99b2f8a08df12
कलविङ्क <zhangx@hzcominfo.com>
2018-12-25 20:00:40 +0800

    enable output format, etl work well.

--- kafka-io/src/main/java/net/butfly/albatis/kafka/KafkaOutput.java
+++ kafka-io/src/main/java/net/butfly/albatis/kafka/KafkaOutput.java
@@ -3,33 +3,25 @@ package net.butfly.albatis.kafka;
 import static net.butfly.albacore.paral.Sdream.of;
 
 import java.util.List;
-import java.util.Map;
-
-import com.hzcominfo.albatis.nosql.Connection;
 
 import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
 import net.butfly.albacore.exception.ConfigException;
 import net.butfly.albacore.io.URISpec;
-import net.butfly.albacore.io.lambda.Function;
 import net.butfly.albacore.paral.Sdream;
+import net.butfly.albacore.utils.collection.Colls;
 import net.butfly.albatis.io.OutputBase;
 import net.butfly.albatis.io.Rmap;
 import net.butfly.albatis.kafka.config.KafkaOutputConfig;
 
-/**
- * @author zx
- */
 public class KafkaOutput extends OutputBase<Rmap> {
 	private static final long serialVersionUID = -7619558227408835825L;
 	private final URISpec uri;
 	private final KafkaOutputConfig config;
 	private final Producer<byte[], byte[]> producer;
-	private Function<Map<String, Object>, byte[]> coder;
 
 	public KafkaOutput(String name, URISpec kafkaURI) throws ConfigException {
 		super(name);
-		this.coder = Connection.uriser(kafkaURI);
 		uri = kafkaURI;
 		config = new KafkaOutputConfig(uri);
 		producer = new Producer<byte[], byte[]>(config.getConfig());
@@ -39,7 +31,11 @@ public class KafkaOutput extends OutputBase<Rmap> {
 	@Override
 	protected void enqsafe(Sdream<Rmap> messages) {
 		List<Rmap> msgs = messages.list();
-		List<KeyedMessage<byte[], byte[]>> ms = of(msgs).map(m -> Kafkas.toKeyedMessage(m, coder)).nonNull().list();
+		List<KeyedMessage<byte[], byte[]>> ms = of(msgs).mapFlat(m -> {
+			List<KeyedMessage<byte[], byte[]>> l = Colls.list();
+			m.forEach((k, body) -> l.add(new KeyedMessage<>(m.table(), k.getBytes(), (byte[]) body)));
+			return of(l);
+		}).nonNull().list();
 		if (!ms.isEmpty()) try {
 			int s = ms.size();
 			if (s == 1) {
@@ -58,10 +54,6 @@ public class KafkaOutput extends OutputBase<Rmap> {
 
 	public String getDefaultTopic() {
 		return config.topics().isEmpty() ? null : config.topics().get(0);
-	}
-
-	public void coder(Function<Map<String, Object>, byte[]> c) {
-		this.coder = c;
 	}
 
 	@Override
---
+++ kafka-io/src/test/java/net/butfly/albatis/kafka/KafkaIOTest.java
@@ -0,0 +1,45 @@
+package net.butfly.albatis.kafka;
+
+import java.util.UUID;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import com.hzcominfo.albatis.in;
+import com.hzcominfo.albatis.out;
+
+import net.butfly.albacore.io.URISpec;
+import net.butfly.albacore.utils.collection.Colls;
+import net.butfly.albatis.io.Rmap;
+
+public class KafkaIOTest {
+	public static void main(String... args) throws Exception {
+		URISpec uri = new URISpec("kafka://data01:2181,data02:2181,data03:2181/kafka");
+		// bson as default and etl as default
+		// input(uri, "flow_PEOPLE_1218_DBRW_inc");
+
+		// bson as default and output as json
+		uri.setParameter("df", "bson,rev:json");
+		input(uri, "flow_PEOPLE_1218_DBRW_inc");
+
+		// string kafka without key (null key)
+		// input("kafka://data01:2181,data02:2181,data03:2181/kafka?df=json", "KAFKA_STRING_TEST");
+		// input("kafka://data01:2181,data02:2181,data03:2181/kafka?df=jsons", "HIK_STR_TEST");
+	}
+
+	public static void input(URISpec uri, String topic) throws Exception {
+		in.main(uri.reauth("KafkaInputTest-" + UUID.randomUUID()).toString(), topic);
+	}
+
+	public static void inputOutput(URISpec uri, String srcTopic, String dstTopic) throws Exception {
+		BlockingQueue<Rmap> q = new LinkedBlockingQueue<>();
+		Thread o = new Thread(() -> out.output(uri, "TEST_ZX_BSON", "id", (t, kf) -> {
+			try {
+				return q.take();
+			} catch (InterruptedException e) {
+				return null;
+			}
+		}));
+		o.start();
+		in.input(uri.reauth("KafkaInputTest-" + UUID.randomUUID()), Colls.list(srcTopic), q::offer);
+	}
+}
