65c921e9bf66f9ade946eb3ee58405a877bcf3a2
कलविङ्क <zhangx@hzcominfo.com>
2019-01-25 14:39:41 +0800

    optimize

--- demo-io/src/main/java/com/hzcominfo/hiktest/StressTest.java
+++ demo-io/src/main/java/com/hzcominfo/hiktest/StressTest.java
@@ -16,6 +16,7 @@ import com.hikvision.multidimensional.kafka.register.schema.KafkaDemo;
 import net.butfly.albacore.io.URISpec;
 import net.butfly.albacore.paral.Sdream;
 import net.butfly.albacore.utils.IOs;
+import net.butfly.albacore.utils.logger.Logger;
 import net.butfly.albatis.Connection;
 import net.butfly.albatis.ddl.TableDesc;
 import net.butfly.albatis.io.Output;
@@ -24,39 +25,44 @@ import net.butfly.albatis.kafka.avro.AvroFormat;
 import net.butfly.albatis.kafka.avro.SchemaReg;
 
 public class StressTest {
+	private static Logger logger = Logger.getLogger(StressTest.class);
 	private static AtomicLong count = new AtomicLong(), spent = new AtomicLong();
 	private static Schema schema = load();
 	private static int paral = Integer.parseInt(System.getProperty("dataggr.migrate.kafka.stress.paral", "-1"));
 	private static long limit = 5 * 10000 * 10000;
+	private static final ExecutorService ex = ex();
 
 	public static void main(String[] args) throws IOException {
 		System.setProperty(SchemaReg.SCHEMA_REGISTRY_URL_CONF, KafkaDemo.SCHEMA_REG);
 		TableDesc dst = AvroFormat.Builder.schema(KafkaDemo.TOPIC, schema);
 
-		ExecutorService ex;
-		if (paral == 0) ex = Executors.newCachedThreadPool();
-		else if (paral == 1) ex = null;
-		else if (paral > 0) ex = Executors.newFixedThreadPool(paral);
-		else if (paral == -1) ex = ForkJoinPool.commonPool();
-		else ex = new ForkJoinPool(-paral);
-		System.err.println("ThreadPool: " + ex);
-
 		try (Connection conn = Connection.connect(new URISpec("kafka2:bootstrap://" + KafkaDemo.BOOTSTRAP + "?df=avro"));
 				Output<Rmap> out = conn.output(dst);) {
 			while (count.get() < limit)
-				if (paral > 1) try {
+				if (null != ex) try {
 					ex.submit(() -> stress1(out));
 				} catch (RejectedExecutionException e) {
-					System.err.println(
-							"Thread pool overflow, incease [dataggr.migrate.kafka.stress.paral] larger than default [10] or set to 0 for infinite.");
+					logger.error(
+							() -> "Thread pool overflow, incease [dataggr.migrate.kafka.stress.paral] larger than default [10] or set to 0 for infinite.");
 					try {
 						Thread.sleep(1000);
 					} catch (InterruptedException e1) {}
 				}
 				else stress1(out);
 		} finally {
-			ex.shutdown();
+			if (null != ex) ex.shutdown();
 		}
+	}
+
+	private static ExecutorService ex() {
+		ExecutorService ex;
+		if (paral == 0) ex = Executors.newCachedThreadPool();
+		else if (paral == 1) ex = null;
+		else if (paral > 0) ex = Executors.newFixedThreadPool(paral);
+		else if (paral == -1) ex = ForkJoinPool.commonPool();
+		else ex = new ForkJoinPool(-paral);
+		logger.info("ThreadPool: " + ex);
+		return ex;
 	}
 
 	private static void stress1(Output<Rmap> out) {
@@ -66,8 +72,8 @@ public class StressTest {
 		} finally {
 			long t = spent.addAndGet(System.currentTimeMillis() - now) / 1000;
 			long c = count.incrementAndGet();
-			if (t > 1 && c % 20000 == 0) //
-				System.err.println("Avro stress sent [" + c + " recs], spent [" + t + " s], avg [" + c / t + " rec/sec].");
+			if (t > 1 && c % 20000 == 0) logger.warn(() -> "Avro stress sent [" + c + " recs], spent [" + t + " s], avg [" + c / t
+					+ " rec/sec]." + (null != ex ? "\n\tpool: " + ex.toString() : ""));;
 		}
 	}
 
@@ -79,8 +85,7 @@ public class StressTest {
 
 	private static Sdream<Rmap> build(Schema schema) {
 		Rmap r = new Rmap(KafkaDemo.TOPIC);
-		for (Schema.Field f : schema.getFields())
-			r.put(f.name(), natual(AvroFormat.Builder.restrictType(f)));
+		schema.getFields().forEach(f -> r.put(f.name(), natual(AvroFormat.Builder.restrictType(f))));
 		return Sdream.of1(r);
 	}
 
