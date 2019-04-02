3e4e903551b24094252455f0d47ecbdc19ecd112
Lucious <lilz@hzcominfo.com>
2019-04-02 18:20:21 +0800

    remove the duplicate prefix

--- core/src/main/java/net/butfly/albatis/ddl/QualifierField.java
+++ core/src/main/java/net/butfly/albatis/ddl/QualifierField.java
@@ -1,11 +1,11 @@
 package net.butfly.albatis.ddl;
 
-import static net.butfly.albatis.ddl.Qualifier.one;
 import static net.butfly.albacore.utils.Texts.eq;
 import static net.butfly.albatis.ddl.FieldDesc.SPLIT_CF;
 import static net.butfly.albatis.ddl.FieldDesc.SPLIT_CF_CH;
 import static net.butfly.albatis.ddl.FieldDesc.SPLIT_PREFIX;
 import static net.butfly.albatis.ddl.FieldDesc.SPLIT_PREFIX_CH;
+import static net.butfly.albatis.ddl.Qualifier.one;
 
 import java.io.Serializable;
 
@@ -13,12 +13,12 @@ public class QualifierField implements Serializable {
 	private static final long serialVersionUID = 5725397514982733160L;
 	public final Qualifier table;
 	public final String name;
+	public final String family;
 	public final String prefix;
 	public final String qualifier;
 
 	/**
-	 * @param field
-	 *            cf:prefix#col
+	 * @param field cf:prefix#col
 	 * @return [(table_name, col_family, col_prefix), col_name]
 	 * @return
 	 */
@@ -27,24 +27,24 @@ public class QualifierField implements Serializable {
 		this.table = new Qualifier(table.name, one(table.family, fqs[0]), one(table.prefix, fqs[1]));
 		this.name = fqs[2];
 
-		this.prefix = (null == table.family ? "" : table.family + SPLIT_CF_CH) + (null == table.prefix ? "" : table.prefix + SPLIT_PREFIX_CH);
+		this.family = null == table.family ? "" : table.family + SPLIT_CF_CH;
+		this.prefix = null == table.prefix ? "" : table.prefix + SPLIT_PREFIX_CH;
 		qualifier = null == name ? prefix : prefix + name;
 	}
 
 	public QualifierField(Qualifier table, String family, String prefix, String field) {
 		this.table = new Qualifier(table.name, one(table.family, family), one(table.prefix, prefix));
 		this.name = field;
-
-		this.prefix = (null == this.table.family ? "" : this.table.family + SPLIT_CF_CH) //
-				+ (null == this.table.prefix ? "" : this.table.prefix + SPLIT_PREFIX_CH);
+		this.family = null == this.table.family ? "" : this.table.family + SPLIT_CF_CH;
+		this.prefix = null == this.table.prefix ? "" : this.table.prefix + SPLIT_PREFIX_CH;
 		qualifier = null == name ? prefix : prefix + name;
 	}
 
 	public QualifierField(String table, String family, String prefix, String field) {
 		this.table = new Qualifier(table, family, prefix);
 		this.name = field;
-
-		this.prefix = (null == family ? "" : family + SPLIT_CF_CH) + (null == prefix ? "" : prefix + SPLIT_PREFIX_CH);
+		this.family = null == this.table.family ? "" : this.table.family + SPLIT_CF_CH;
+		this.prefix = null == this.table.prefix ? "" : this.table.prefix + SPLIT_PREFIX_CH;
 		qualifier = null == name ? prefix : prefix + name;
 	}
 
@@ -53,7 +53,9 @@ public class QualifierField implements Serializable {
 	}
 
 	public QualifierField prefix(boolean enable) {
-		return enable ? this : new QualifierField(table, table.family, null, null == prefix ? name : prefix + SPLIT_PREFIX_CH + name);
+		return enable ? this
+				: new QualifierField(table, table.family, null,
+						null == prefix ? name : prefix + SPLIT_PREFIX_CH + name);
 	}
 
 	@Override
@@ -63,8 +65,10 @@ public class QualifierField implements Serializable {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (obj == this) return true;
-		if (null == obj || !(obj instanceof QualifierField)) return false;
+		if (obj == this)
+			return true;
+		if (null == obj || !(obj instanceof QualifierField))
+			return false;
 		QualifierField q = (QualifierField) obj;
 		return eq(name, q.name) && (table == q.table || (null != table && table.equals(q.table))/* same table */);
 	}
@@ -77,7 +81,8 @@ public class QualifierField implements Serializable {
 	// cf#prefix:colkey
 	private static String[] parseFieldName(String fq) {
 		String[] fqs = new String[3];
-		if (null == fq) return fqs;
+		if (null == fq)
+			return fqs;
 		fqs[2] = fq;
 		String[] cfs = fqs[2].split(SPLIT_CF, 2); // cf and prefix#colkey
 		if (cfs.length == 2) {
