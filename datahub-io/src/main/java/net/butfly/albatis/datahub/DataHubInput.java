9f8635b0b7335c86e059e39ba292d73f6824308c
lixl <lixl@hzcominfo.com>
2019-05-20 10:25:00 +0800

    modify no thread safe

--- datahub-io/src/main/java/net/butfly/albatis/datahub/DataHubInput.java
+++ datahub-io/src/main/java/net/butfly/albatis/datahub/DataHubInput.java
@@ -1,6 +1,8 @@
 package net.butfly.albatis.datahub;
 
 import java.io.IOException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import org.apache.commons.codec.Charsets;
 
@@ -19,32 +21,43 @@ public class DataHubInput extends net.butfly.albacore.base.Namedly implements Od
 	private static final long serialVersionUID = 2242853649760090074L;
 	//private static final Logger logger = Logger.getLogger(DataHubInput.class);
 	
+	private final BlockingQueue<Consumer> consumers;
+	
 	private final Consumer consumer;
+	
+	{
+		consumers = new LinkedBlockingQueue<>();
+	}
 	
 	public DataHubInput(String name, URISpec uri, String... tables) throws IOException {
 		super(name);
 		ConsumerConfig config = new ConsumerConfig("http://"+uri.getHost(), uri.getUsername(), uri.getPassword());
 		consumer = new Consumer(uri.getFile(), tables[0], "1", config);
+		consumers.add(consumer);
 		closing(this::close);
 	}
 
 	@Override
 	public Rmap dequeue(){
 		while (opened()) {
-			Rmap rs =  new Rmap();
-			RecordEntry entry = consumer.read(1);
-			RecordData rdata = entry.getRecordData();
-			if(rdata == null ) return null;
-			if (rdata instanceof TupleRecordData) {
-				TupleRecordData data = (TupleRecordData) rdata;
-				data.getRecordSchema().getFields().forEach(f ->{
-					rs.put(f.getName(), data.getField(f.getName()));
-				});
-			}else {
-				BlobRecordData data = (BlobRecordData) rdata;
-				rs.put("BlobRecordData", new String(data.getData(), Charsets.UTF_8));
+			Consumer c = consumers.poll();
+			if(null != c) {
+				Rmap rs =  new Rmap();
+				RecordEntry entry = c.read(1);
+				consumers.add(c);
+				RecordData rdata = entry.getRecordData();
+				if(rdata == null ) return null;
+				if (rdata instanceof TupleRecordData) {
+					TupleRecordData data = (TupleRecordData) rdata;
+					data.getRecordSchema().getFields().forEach(f ->{
+						rs.put(f.getName(), data.getField(f.getName()));
+					});
+				}else {
+					BlobRecordData data = (BlobRecordData) rdata;
+					rs.put("BlobRecordData", new String(data.getData(), Charsets.UTF_8));
+				}
+				return rs;	
 			}
-			return rs;	
 		}
 		return null;
 	}
