ef9abff8dada7731006ec2181bdc6ed22e4f7f08
HZCOMINFO\liulp <liulp@hzcominfo.com>
2019-05-31 20:03:42 +0800

    replace httpclient method

--- http-io/src/main/java/net/butfly/albatis/http/HttpConnection.java
+++ http-io/src/main/java/net/butfly/albatis/http/HttpConnection.java
@@ -3,21 +3,33 @@ package net.butfly.albatis.http;
 import java.io.IOException;
 
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.HttpClientBuilder;
 
 import net.butfly.albacore.io.URISpec;
 import net.butfly.albatis.DataConnection;
 
-public class HttpConnection extends DataConnection<HttpClient>{
+public class HttpConnection extends DataConnection<HttpClient> {
+	public HttpClient httpClient;
 
-	public HttpConnection(URISpec uri) throws IOException{
-		this(uri,"http");
-	}
-	
-	public HttpConnection(URISpec uri, String... param) throws IOException {
-		super(uri,param);
+	public HttpConnection(URISpec uri) throws IOException {
+		super(uri);
 	}
 
-	public HttpInput input(String... tables) {
-		return new HttpInput("HttpInut", uri, tables);		
+	public HttpConnection(String urispec) throws IOException {
+		this(new URISpec(urispec));
+	}
+
+	protected HttpClient initialize(URISpec uri) {
+
+		RequestConfig defaultRequestConfig = RequestConfig.custom().setConnectTimeout(30000)
+				.setConnectionRequestTimeout(30000).setSocketTimeout(30000).build();
+		httpClient = (HttpClient) HttpClientBuilder.create().setDefaultRequestConfig(defaultRequestConfig);
+
+		return httpClient;
+	}
+
+	public HttpInput inputRaw() {
+		return new HttpInput("HttpInput", httpClient);
 	}
 }
--- http-io/src/main/java/net/butfly/albatis/http/HttpInput.java
+++ http-io/src/main/java/net/butfly/albatis/http/HttpInput.java
@@ -1,12 +1,17 @@
 package net.butfly.albatis.http;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.util.EntityUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import net.butfly.albacore.io.URISpec;
 import net.butfly.albatis.io.OddInput;
@@ -16,70 +21,50 @@ public class HttpInput extends net.butfly.albacore.base.Namedly implements OddIn
 	private static final long serialVersionUID = -7766621067006571958L;
 	public String table;
 	public URISpec url;
-	public String param;
+	private static HttpClient client;
 
-	@SuppressWarnings("deprecation")
-	public HttpInput(String table, URISpec url, String... param) {
-		super();
-		this.name = table;
+	public HttpInput(String name, HttpClient client) {
+		super(name);
+		this.client = client;
 	}
 
-	private String httpPost(String table, URISpec uri, String param) {
-		String Url = url + "?manufactorID=12332&sendTime=" + param + "&dataType=1&token=xdjfjjdjdjdd43333";
-		PrintWriter out = null;
-		InputStream is = null;
-		BufferedReader br = null;
-		String result = "";
-		HttpURLConnection conn = null;
-		StringBuffer strBuffer = new StringBuffer();
-		try {
-			URL realUrl = new URL(Url);
-			conn = (HttpURLConnection) realUrl.openConnection();
-			conn.setRequestMethod("POST");
-			conn.setConnectTimeout(20000);
-			conn.setReadTimeout(300000);
-			conn.setRequestProperty("Charset", "UTF-8");
-			conn.setRequestProperty("Content-Type", "application/json");
-			conn.setRequestProperty("Content-Encoding", "utf-8");
-			conn.setDoOutput(true);
-			conn.setDoInput(true);
-			conn.setUseCaches(false);
-			out = new PrintWriter(conn.getOutputStream());
-			out.print(param);
-			out.flush();
-			is = conn.getInputStream();
-			br = new BufferedReader(new InputStreamReader(is));
-			String line = null;
-			while ((line = br.readLine()) != null) {
-				strBuffer.append(line);
-			}
-			result = strBuffer.toString();
-		} catch (Exception e) {
-			System.out.println("发送 POST 请求出现异常！" + e);
-			e.printStackTrace();
-		} finally {
-			try {
-				if (out != null) {
-					out.close();
-				}
-				if (br != null) {
-					br.close();
-				}
-				if (conn != null) {
-					conn.disconnect();
-				}
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
+	public static ThreadLocal<ObjectMapper> httpGetMapper = new ThreadLocal<ObjectMapper>() {
+		@Override
+		protected ObjectMapper initialValue() {
+			return new ObjectMapper();
 		}
-		return result;
+	};
+	public static ThreadLocal<ObjectMapper> httpPostMapper = new ThreadLocal<ObjectMapper>() {
+		@Override
+		protected ObjectMapper initialValue() {
+			return new ObjectMapper();
+		}
+	};
+
+	private static String httpPost(URISpec uri) {
+		SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
+		String now = df.format(new Date());
+		String Url = "http://" + uri.getHost() + ":" + uri.getDefaultPort() + uri.getUsername() + "?"
+				+ uri.getPassword() + now;
+		HttpPost httppost = new HttpPost(Url);
+		HttpResponse response = null;
+		try {
+			response = client.execute(httppost, HttpClientContext.create());
+			HttpEntity entity = response.getEntity();
+			String result = EntityUtils.toString(entity, "utf-8");
+			return result;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return null;
 	}
 
 	@SuppressWarnings("deprecation")
 	@Override
 	public Rmap dequeue() {
 		while (opened()) {
-			String m = httpPost(table, url, param);
+			String m = httpPost(url);
 			return new Rmap(table, m);
 		}
 		return null;
