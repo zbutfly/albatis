3408838924ef7dd303219727571aa115ed27e7dd
Lucious <lilvzhou@hikvision.com.cn>
2019-08-26 20:04:14 +0800

    optimization
    optimization
    
    
    update cassandra  api version and the core version.

--- cassandra-io/src/main/java/net/butfly/albatis/cassandra/CassandraOutput.java
+++ cassandra-io/src/main/java/net/butfly/albatis/cassandra/CassandraOutput.java
@@ -1,31 +1,33 @@
 package net.butfly.albatis.cassandra;
 
+import static com.datastax.driver.core.querybuilder.QueryBuilder.eq;
+import static com.datastax.driver.core.querybuilder.QueryBuilder.set;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.ResultSetFuture;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.querybuilder.Batch;
 import com.datastax.driver.core.querybuilder.Insert;
 import com.datastax.driver.core.querybuilder.QueryBuilder;
-
+import com.datastax.driver.core.querybuilder.Update;
 
 import net.butfly.albacore.io.URISpec;
 import net.butfly.albacore.paral.Sdream;
-
+import net.butfly.albacore.utils.Configs;
+import net.butfly.albacore.utils.collection.Colls;
 import net.butfly.albatis.io.OutputBase;
 import net.butfly.albatis.io.Rmap;
 
+public class CassandraOutput extends OutputBase<Rmap> {
 
-
-public class CassandraOutput extends OutputBase<Rmap>{
-	
+	private static final long serialVersionUID = 5480193154210194139L;
 	private final CassandraConnection Caconn;
 	private final String keyspace;
 	private final String table_name;
 	private Session session;
-	
-
+	public static boolean BATCH_INSERT = Boolean.parseBoolean(Configs.gets("albatis.cassandra.batch.insert", "true"));
 
 	public CassandraOutput(String name, String keyspace, String table_name, CassandraConnection Caconn) {
 		super(name);
@@ -35,48 +37,66 @@ public class CassandraOutput extends OutputBase<Rmap>{
 		this.session = Caconn.client.connect();
 		closing(this.Caconn::close);
 	}
-	
-	
-	public void createTable(String space_name,String table_name, Session session) {
+
+	public void createTable(String space_name, String table_name, Session session) {
 		StringBuilder cql = new StringBuilder();
-		cql.append("CREATE TABLE if not exists ").append(space_name).append(".").append(table_name)
-		.append("(");
-		
-		
+		cql.append("CREATE TABLE if not exists ").append(space_name).append(".").append(table_name).append("(");
 	}
 
 	@Override
 	protected void enqsafe(Sdream<Rmap> items) {
-		
+		List<Rmap> failedList = Colls.list();
 		List<Rmap> msgs = items.collect();
-		//createTable(keyspace,table_name,session);
-		if(msgs.size() > 0) {
-			try  {
-				msgs.forEach(m -> upsert(keyspace, table_name, session, m));
-				
+		if (BATCH_INSERT) {
+			Batch localBatch = QueryBuilder.unloggedBatch();
+			items.map(i -> {
+				Insert qb = QueryBuilder.insertInto(keyspace, i.table().name);
+				i.forEach((k, v) -> qb.value(k, v));
+				localBatch.add(qb);
+				return qb;
+			}).collect();
+			ResultSetFuture rs = session.executeAsync(localBatch);
+//			try {
+//				rs.get();
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			} catch (ExecutionException e) {
+//				e.printStackTrace();
+//			}
+//			System.out.print(rs);
+		} else {
+			msgs.forEach(m -> {try {
+				upsert(m);	
 			} catch (Exception e) {
-				logger().error("failed to insert or update items.");
-			}
-		}else {
-		   return;
+				failedList.add(m);
+				logger().error("failed to insert or update items.", e);
+			}});
 		}
-		
 	}
-	
-	public void upsert(String spacename, String table_name, Session session, Rmap rmap) {
-		List<String> keyList = new ArrayList<>();
-		List<Object> valueList = new ArrayList<>();
-	
-		rmap.map().forEach((k, v) -> {keyList.add(k);valueList.add(v);});
 
-		Insert insert = QueryBuilder.insertInto(spacename, table_name).values(keyList, valueList);
-		System.out.println(insert); 
-		session.execute(insert);
+	public void upsert(Rmap rmap) {
+		Update update = QueryBuilder.update(keyspace, rmap.table().name);
+		rmap.map().forEach((k, v) -> {
+			update.with(set(k, v));
+		});
+		update.where(eq(rmap.keyField(), rmap.key()));
+		ResultSet ur = session.execute(update);
+		boolean applied = ur.wasApplied();
+		System.out.print(ur);
+		if (!applied) {
+			Insert insert = QueryBuilder.insertInto(keyspace, table_name);
+			rmap.map().forEach((k, v) -> {
+				insert.value(k, v);
+			});
+			System.out.println(insert);
+			ResultSet ir = session.execute(insert);
+			System.out.print(ir);
+		}
 	}
 
 	@Override
 	public URISpec target() {
 		return Caconn.uri();
 	}
-	
+
 }
