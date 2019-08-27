00820bd9c2e7087fc5d8e40621b51b3f5acaf636
Lucious <lilvzhou@hikvision.com.cn>
2019-08-27 17:30:16 +0800

    optimize cassandra output, add batch execution and execution pool.

--- cassandra-io/src/main/java/net/butfly/albatis/cassandra/CassandraOutput.java
+++ cassandra-io/src/main/java/net/butfly/albatis/cassandra/CassandraOutput.java
@@ -4,6 +4,11 @@ import static com.datastax.driver.core.querybuilder.QueryBuilder.eq;
 import static com.datastax.driver.core.querybuilder.QueryBuilder.set;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.ResultSetFuture;
@@ -25,17 +30,27 @@ public class CassandraOutput extends OutputBase<Rmap> {
 	private static final long serialVersionUID = 5480193154210194139L;
 	private final CassandraConnection Caconn;
 	private final String keyspace;
-	private final String table_name;
+//	private final String table_name;
 	private Session session;
 	public static boolean BATCH_INSERT = Boolean.parseBoolean(Configs.gets("albatis.cassandra.batch.insert", "true"));
+	private static final ForkJoinPool EXPOOL_CA = //
+            new ForkJoinPool(Integer.parseInt(Configs.gets("albatis.cassandra.batch.insert.paral", "5")), ForkJoinPool.defaultForkJoinWorkerThreadFactory, null, true);
+	private int INSERT_SIZE = Integer.parseInt(Configs.gets("albatis.cassandra.batch.insert.size", "500"));
+	private long NOTENOUGH_WAIT = Long.parseLong(Configs.gets("albatis.cassandra.batch.notEnough.wait", "30000"));
+	private int EMPTY_WAIT = Integer.parseInt(Configs.gets("albatis.cassandra.batch.insert.wait", "100"));
+	private AtomicBoolean STOPPED = new AtomicBoolean(false);
+	private AtomicLong count = new AtomicLong();
+	public final LinkedBlockingQueue<Rmap> lbq = new LinkedBlockingQueue<>(50000);
 
 	public CassandraOutput(String name, String keyspace, String table_name, CassandraConnection Caconn) {
 		super(name);
 		this.Caconn = Caconn;
 		this.keyspace = keyspace;
-		this.table_name = table_name;
+//		this.table_name = table_name;
 		this.session = Caconn.client.connect();
-		closing(this.Caconn::close);
+		Thread thread = new Thread(new InsertTask());
+		thread.start();
+		closing(this::close);
 	}
 
 	public void createTable(String space_name, String table_name, Session session) {
@@ -48,14 +63,14 @@ public class CassandraOutput extends OutputBase<Rmap> {
 		List<Rmap> failedList = Colls.list();
 		List<Rmap> msgs = items.collect();
 		if (BATCH_INSERT) {
-			Batch localBatch = QueryBuilder.unloggedBatch();
-			items.map(i -> {
-				Insert qb = QueryBuilder.insertInto(keyspace, i.table().name);
-				i.forEach((k, v) -> qb.value(k, v));
-				localBatch.add(qb);
-				return qb;
-			}).collect();
-			ResultSetFuture rs = session.executeAsync(localBatch);
+//			Batch localBatch = QueryBuilder.unloggedBatch();
+//			items.map(i -> {
+//				Insert qb = QueryBuilder.insertInto(keyspace, i.table().name);
+//				i.forEach((k, v) -> qb.value(k, v));
+//				localBatch.add(qb);
+//				return qb;
+//			}).collect();
+//			ResultSetFuture rs = session.executeAsync(localBatch);
 //			try {
 //				rs.get();
 //			} catch (InterruptedException e) {
@@ -63,6 +78,13 @@ public class CassandraOutput extends OutputBase<Rmap> {
 //			} catch (ExecutionException e) {
 //				e.printStackTrace();
 //			}
+			items.eachs(i -> {
+				try {
+					lbq.put(i);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			});
 //			System.out.print(rs);
 		} else {
 			msgs.forEach(m -> {try {
@@ -74,6 +96,19 @@ public class CassandraOutput extends OutputBase<Rmap> {
 		}
 	}
 
+	@Override
+	public void close() {
+		while (!lbq.isEmpty()) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+			}
+		}
+		STOPPED.set(true);
+		EXPOOL_CA.shutdown();
+		session.close();
+	}
+	
 	public void upsert(Rmap rmap) {
 		Update update = QueryBuilder.update(keyspace, rmap.table().name);
 		rmap.map().forEach((k, v) -> {
@@ -84,7 +119,7 @@ public class CassandraOutput extends OutputBase<Rmap> {
 		boolean applied = ur.wasApplied();
 		System.out.print(ur);
 		if (!applied) {
-			Insert insert = QueryBuilder.insertInto(keyspace, table_name);
+			Insert insert = QueryBuilder.insertInto(keyspace, rmap.table().name);
 			rmap.map().forEach((k, v) -> {
 				insert.value(k, v);
 			});
@@ -94,6 +129,46 @@ public class CassandraOutput extends OutputBase<Rmap> {
 		}
 	}
 
+	private class InsertTask implements Runnable {
+
+		@Override
+		public void run() {
+			long lastTime = System.currentTimeMillis();
+			while (!STOPPED.get()) {
+				List<Rmap> l = Colls.list();
+				long now = System.currentTimeMillis();
+				if (lbq.size() > INSERT_SIZE || (lbq.size() > 0 && now - lastTime > NOTENOUGH_WAIT)) {
+					lastTime = now;
+					lbq.drainTo(l, INSERT_SIZE);
+					EXPOOL_CA.submit(() -> {
+						Batch localBatch = QueryBuilder.unloggedBatch();
+						l.forEach(i -> {
+							Insert qb = QueryBuilder.insertInto(keyspace, i.table().name);
+							i.forEach((k, v) -> qb.value(k, v));
+							localBatch.add(qb);
+						});
+						ResultSetFuture rs = session.executeAsync(localBatch);
+						try {
+							rs.get();
+							System.out.println("count" + count.addAndGet(l.size()));
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						} catch (ExecutionException e) {
+							e.printStackTrace();
+						}
+					});
+				} else {
+					try {
+						Thread.sleep(EMPTY_WAIT);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		
+	}
+	
 	@Override
 	public URISpec target() {
 		return Caconn.uri();
