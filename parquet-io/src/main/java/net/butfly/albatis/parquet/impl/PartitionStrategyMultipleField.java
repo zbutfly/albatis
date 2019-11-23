f54836c34339006e21ef6186f8ec2a8cf5418a26
कलविङ्क <zhangx@hzcominfo.com>
2019-11-23 18:30:03 +0800

    adjust partition params into new style (use fel expr)

--- parquet-io/src/main/java/net/butfly/albatis/parquet/impl/PartitionStrategyMultipleField.java
+++ parquet-io/src/main/java/net/butfly/albatis/parquet/impl/PartitionStrategyMultipleField.java
@@ -1,14 +1,12 @@
 package net.butfly.albatis.parquet.impl;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.hadoop.fs.Path;
 
+import net.butfly.albacore.expr.Engine;
 import net.butfly.albacore.utils.collection.Colls;
 import net.butfly.albatis.ddl.ExtraTableDesc.ExtraEnabled;
 
@@ -34,55 +32,39 @@ public class PartitionStrategyMultipleField extends PartitionStrategy {
 
 	@Override
 	public String partition(Map<String, Object> rec) {
-		if (null == rec || rec.isEmpty()) throw new IllegalArgumentException("Partition failed on value null.");
+		if (null == rec || rec.isEmpty()) throw new IllegalArgumentException("Partition failed on value null or empty.");
 		List<String> ss = new ArrayList<>();
-		Object v;
-		for (Segment seg : segments) {
-			if (null == (v = rec.get(seg.srcFieldName))) //
-				throw new IllegalArgumentException("Partition failed on value null for field: " + seg.partitionFieldName);
-			ss.add(seg.partitionFieldName + "=" + seg(v, seg.partitionFormat, seg.parseFormat));
-		}
+		for (Segment seg : segments) ss.add(seg.seg(rec));
 		return String.join(Path.SEPARATOR, ss);
-	}
-
-	private String seg(Object value, String partitionFormat, String parseFormat) {
-		Class<? extends Object> c = value.getClass();
-		SimpleDateFormat f = new SimpleDateFormat(partitionFormat);
-		if (Date.class.isAssignableFrom(c)) return f.format(value);
-		if (Number.class.isAssignableFrom(c)) return f.format(new Date(((Number) value).longValue()));
-		if (CharSequence.class.isAssignableFrom(c)) if (null == parseFormat) //
-			throw new IllegalArgumentException("Partition failed for string value without parseing format.");
-		else try {
-			return f.format(new SimpleDateFormat(parseFormat).parse(value.toString()));
-		} catch (ParseException e) {
-			throw new IllegalArgumentException("Partition failed with format [" + parseFormat + "] failed on value: " + value.toString());
-		}
-		return null;
 	}
 
 	public static class Segment {
 		public final int level;
 		public final String partitionFieldName;
-		public final FieldType partitionFieldType;
-		public final String partitionFormat;
-
-		public final String srcFieldName;
-		public final FieldType srcFieldType;
-		public final String parseFormat;
+		public final String srcField;
 
 		public Segment(Map<String, Object> json) {
 			super();
 			level = ((Number) json.get("level")).intValue();
-			parseFormat = (String) json.get("parseFormat");
 			partitionFieldName = (String) json.get("partitionFieldName");
-			partitionFieldType = FieldType.valueOf(((String) json.get("partitionFieldType")).toUpperCase());
-			partitionFormat = (String) json.get("partitionFormat");
-			srcFieldName = (String) json.get("srcFieldName");
-			srcFieldType = FieldType.valueOf(((String) json.get("srcFieldType")).toUpperCase());
+			String expr = (String) json.get("srcField");
+			if (expr.startsWith("=")) expr = expr.substring(1);
+			srcField = expr;
+		}
+
+		public String seg(Map<String, Object> rec) {
+			return partitionFieldName + "=" + Engine.eval(srcField, rec);
+			// Class<? extends Object> c = value.getClass();
+			// if (Date.class.isAssignableFrom(c)) return f.format(value);
+			// if (Number.class.isAssignableFrom(c)) return f.format(new Date(((Number) value).longValue()));
+			// if (CharSequence.class.isAssignableFrom(c)) // if (null == parseFormat) //
+			// throw new IllegalArgumentException("Partition failed for string value without parseing format.");
+			// else try {
+			// return f.format(new SimpleDateFormat(parseFormat).parse(value.toString()));
+			// } catch (ParseException e) {
+			// throw new IllegalArgumentException("Partition failed without format on value: " + value.toString());
+			// }
 		}
 	}
 
-	private enum FieldType {
-		STRING, DATE, LONG
-	}
 }
